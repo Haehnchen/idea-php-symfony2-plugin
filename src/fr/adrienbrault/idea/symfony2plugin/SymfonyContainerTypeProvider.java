904d425034b7372778f17d4c198b99345b7d2a96
Adrien Brault <adrien.brault@gmail.com>
2013-04-09 03:31:53 +0200

    Support all ContainerInterface::get proxy as long as the id is the first argument

--- src/fr/adrienbrault/idea/symfony2plugin/SymfonyContainerTypeProvider.java
+++ src/fr/adrienbrault/idea/symfony2plugin/SymfonyContainerTypeProvider.java
@@ -3,6 +3,8 @@ package fr.adrienbrault.idea.symfony2plugin;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.codeInsight.controlFlow.instructions.PhpInstruction;
+import com.jetbrains.php.codeInsight.controlFlow.instructions.PhpReturnInstruction;
 import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
@@ -57,6 +59,11 @@ public class SymfonyContainerTypeProvider implements PhpTypeProvider {
     }
 
     private boolean isContainerGetCall(PsiElement e) {
+        return isContainerGetCall(e, 1);
+    }
+
+    private boolean isContainerGetCall(PsiElement e, int deepness) {
+        // ((Method)((MethodReference)((PhpReturnInstruction)((Method)((MethodReference)e).getReference().resolve()).getControlFlow().getInstructions()[6]).getArgument()).getReference().resolve()).getFQN()
         if (!(e instanceof  MethodReference)) {
             return false;
         }
@@ -77,9 +84,31 @@ public class SymfonyContainerTypeProvider implements PhpTypeProvider {
             return false;
         }
 
-        if (methodFQN.equals("\\Symfony\\Bundle\\FrameworkBundle\\Controller\\Controller.get")
-            || methodFQN.equals("\\Symfony\\Component\\DependencyInjection\\ContainerInterface.get")) {
+        String expectedMethodFQN = "\\Symfony\\Component\\DependencyInjection\\ContainerInterface.get";
+        if (methodFQN.equals(expectedMethodFQN)) {
             return true;
+        }
+
+        if (deepness > 3) {
+            return false;
+        }
+
+        // Try to see if this method return expression is a method call to a ContainerInterface::get ... recursive!
+
+        PhpInstruction[] instructions = method.getControlFlow().getInstructions();
+        for (int i = 0; i < instructions.length; i++) {
+            PhpInstruction instruction = instructions[i];
+
+            if (instruction instanceof PhpReturnInstruction) {
+                PhpReturnInstruction returnInstruction = (PhpReturnInstruction) instruction;
+
+                PsiElement returnInstructionElement = returnInstruction.getArgument();
+                if (null != returnInstructionElement &&
+                    null != returnInstructionElement.getReference() &&
+                    returnInstructionElement.getReference().resolve() != resolvedReference) { // Avoid stackoverflow with method calling itself
+                    return isContainerGetCall(returnInstructionElement, deepness + 1);
+                }
+            }
         }
 
         return false;
