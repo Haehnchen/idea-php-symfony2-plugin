ac400c4338090b621a9f20a35156374a1e78ce37
Daniel Espendiller <espendiller@gmx.de>
2013-06-06 17:15:24 +0200

    better use PhpCodeInsightUtil.toString to strip quote values fix #69

--- src/fr/adrienbrault/idea/symfony2plugin/config/xml/provider/ServiceReferenceProvider.java
+++ src/fr/adrienbrault/idea/symfony2plugin/config/xml/provider/ServiceReferenceProvider.java
@@ -5,6 +5,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiReferenceProvider;
 import com.intellij.util.ProcessingContext;
+import com.jetbrains.php.codeInsight.PhpCodeInsightUtil;
 import fr.adrienbrault.idea.symfony2plugin.dic.ServiceReference;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,9 +14,12 @@ public class ServiceReferenceProvider extends PsiReferenceProvider {
     @NotNull
     @Override
     public PsiReference[] getReferencesByElement(@NotNull PsiElement psiElement, @NotNull ProcessingContext processingContext) {
+
         // get the service name "service_container"
-        String text = psiElement.getText();
-        text = text.substring(1, text.length() - 1);
+        String text = PhpCodeInsightUtil.toString(psiElement);
+        if(text == null) {
+            return new PsiReference[0];
+        }
 
         return new PsiReference[]{ new ServiceReference(psiElement, text) };
     }
