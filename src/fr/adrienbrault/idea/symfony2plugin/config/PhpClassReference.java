c36dde116c63e49035b924f2ad157ee43f5943a3
Daniel Espendiller <espendiller@gmx.de>
2013-08-02 18:41:05 +0200

    PhpClassReference can provide variants too

--- src/fr/adrienbrault/idea/symfony2plugin/config/PhpClassReference.java
+++ src/fr/adrienbrault/idea/symfony2plugin/config/PhpClassReference.java
@@ -1,10 +1,14 @@
 package fr.adrienbrault.idea.symfony2plugin.config;
 
+import com.intellij.codeInsight.completion.impl.CamelHumpMatcher;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.psi.*;
 import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.completion.PhpClassLookupElement;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import fr.adrienbrault.idea.symfony2plugin.util.PsiElementUtils;
+import fr.adrienbrault.idea.symfony2plugin.util.completion.PhpClassReferenceInsertHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,15 +20,21 @@ import java.util.*;
 public class PhpClassReference extends PsiReferenceBase<PsiElement> implements PsiPolyVariantReference {
 
     private String classFQN;
+    private boolean provideVariants = false;
 
     public PhpClassReference(@NotNull StringLiteralExpression element) {
         super(element);
-        this.classFQN = element.getContents();
+        this.classFQN = PsiElementUtils.getMethodParameter(element);
     }
 
     public PhpClassReference(@NotNull PsiElement element, String classFQN) {
         super(element);
         this.classFQN = classFQN;
+    }
+
+    public PhpClassReference(@NotNull StringLiteralExpression element, boolean provideVariants) {
+        this(element);
+        this.provideVariants = provideVariants;
     }
 
     @NotNull
@@ -52,8 +62,23 @@ public class PhpClassReference extends PsiReferenceBase<PsiElement> implements P
     @NotNull
     @Override
     public Object[] getVariants() {
+
         List<LookupElement> results = new ArrayList<LookupElement>();
-        // fill this
+
+        // not break old calls to this
+        // @TODO: check performance and migrate all custom completion to this method
+        if(!this.provideVariants) {
+            return results.toArray();
+        }
+
+        PhpIndex phpIndex = PhpIndex.getInstance(this.getElement().getProject());
+        for (String name : phpIndex.getAllClassNames(new CamelHumpMatcher(this.classFQN))) {
+            Collection<PhpClass> classes = phpIndex.getClassesByName(name);
+            for(PhpClass phpClass: classes) {
+                results.add(new PhpClassLookupElement(phpClass, true, PhpClassReferenceInsertHandler.getInstance()));
+            }
+        }
+
         return results.toArray();
     }
 }
