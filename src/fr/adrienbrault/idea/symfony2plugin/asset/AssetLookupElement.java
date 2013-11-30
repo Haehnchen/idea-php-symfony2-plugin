cd71d5a28fbb86d26c9c55ce5a8bf4cc55922cc5
Daniel Espendiller <espendiller@gmx.de>
2013-11-30 14:25:25 +0100

    prevent double "@" on twig assetic completion #185

--- src/fr/adrienbrault/idea/symfony2plugin/asset/AssetLookupElement.java
+++ src/fr/adrienbrault/idea/symfony2plugin/asset/AssetLookupElement.java
@@ -1,11 +1,13 @@
 package fr.adrienbrault.idea.symfony2plugin.asset;
 
+import com.intellij.codeInsight.completion.InsertionContext;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.IconUtil;
 import fr.adrienbrault.idea.symfony2plugin.asset.dic.AssetEnum;
 import fr.adrienbrault.idea.symfony2plugin.asset.dic.AssetFile;
+import fr.adrienbrault.idea.symfony2plugin.util.dict.ResourceFileInsertHandler;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -23,7 +25,20 @@ public class AssetLookupElement extends LookupElement {
     @NotNull
     @Override
     public String getLookupString() {
+
+        if(assetFile.getAssetPosition().equals(AssetEnum.Position.Bundle)) {
+            return assetFile.toString().substring(1);
+        }
+
         return assetFile.toString();
+    }
+
+    @Override
+    public void handleInsert(InsertionContext context) {
+        if(assetFile.getAssetPosition().equals(AssetEnum.Position.Bundle)) {
+            ResourceFileInsertHandler.getInstance().handleInsert(context, this);
+        }
+        super.handleInsert(context);
     }
 
     @Override
@@ -34,7 +49,7 @@ public class AssetLookupElement extends LookupElement {
             typeText = "Bundle";
         }
 
-        presentation.setItemText(getLookupString());
+        presentation.setItemText(assetFile.toString());
         presentation.setTypeText(typeText);
         presentation.setIcon(IconUtil.getIcon(assetFile.getFile(), 0, project));
 
