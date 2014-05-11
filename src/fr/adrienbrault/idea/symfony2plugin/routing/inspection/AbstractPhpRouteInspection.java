28fe66fd0c294de03b86ca6511de819f2e88fcc3
Daniel Espendiller <espendiller@gmx.de>
2014-05-11 16:40:11 +0200

    allow independent languages files for route inspection; twig too slow for active now

--- src/fr/adrienbrault/idea/symfony2plugin/routing/inspection/AbstractPhpRouteInspection.java
+++ src/fr/adrienbrault/idea/symfony2plugin/routing/inspection/AbstractPhpRouteInspection.java
@@ -1,58 +1,67 @@
 package fr.adrienbrault.idea.symfony2plugin.routing.inspection;
 
 import com.intellij.codeInspection.LocalInspectionTool;
-import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiRecursiveElementWalkingVisitor;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.indexing.FileBasedIndex;
-import com.jetbrains.php.lang.PhpFileType;
+import com.jetbrains.php.lang.psi.PhpFile;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
-import fr.adrienbrault.idea.symfony2plugin.Settings;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
 import fr.adrienbrault.idea.symfony2plugin.routing.PhpRouteReferenceContributor;
-import fr.adrienbrault.idea.symfony2plugin.routing.Route;
-import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.AnnotationRoutesStubIndex;
-import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.YamlRoutesStubIndex;
 import fr.adrienbrault.idea.symfony2plugin.util.MethodMatcher;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.yaml.YAMLFileType;
-
-import java.util.Collection;
-import java.util.Map;
 
 abstract public class AbstractPhpRouteInspection extends LocalInspectionTool {
+
+    @Override
+    public boolean runForWholeFile() {
+        return true;
+    }
 
     @NotNull
     @Override
     public PsiElementVisitor buildVisitor(final @NotNull ProblemsHolder holder, boolean isOnTheFly) {
 
         PsiFile psiFile = holder.getFile();
-        if(!Symfony2ProjectComponent.isEnabled(psiFile.getProject()) || !Settings.getInstance(psiFile.getProject()).phpAnnotateRoute) {
+        if(!Symfony2ProjectComponent.isEnabled(psiFile.getProject())) {
             return super.buildVisitor(holder, isOnTheFly);
         }
 
-        psiFile.acceptChildren(new PsiRecursiveElementWalkingVisitor() {
-            @Override
-            public void visitElement(PsiElement element) {
-                if(element instanceof StringLiteralExpression) {
-                    annotate((StringLiteralExpression) element, holder);
+        if(psiFile instanceof PhpFile) {
+            psiFile.acceptChildren(new PsiRecursiveElementWalkingVisitor() {
+                @Override
+                public void visitElement(PsiElement element) {
+                    if(element instanceof StringLiteralExpression) {
+                        annotate(((StringLiteralExpression) element).getContents(), element, holder);
+                    }
+                    super.visitElement(element);
                 }
-                super.visitElement(element);
-            }
-        });
+            });
+        }
 
+        /* too slow
+        if(psiFile instanceof TwigFile) {
+            psiFile.acceptChildren(new PsiRecursiveElementWalkingVisitor() {
+                @Override
+                public void visitElement(PsiElement element) {
+
+                    if(TwigHelper.getAutocompletableRoutePattern().accepts(element)) {
+                        annotateRouteName(element, holder, element.getText());
+                    }
+
+                    super.visitElement(element);
+                }
+            });
+        }*/
 
         return super.buildVisitor(holder, isOnTheFly);
     }
 
-    public void annotate(@NotNull final StringLiteralExpression element, @NotNull ProblemsHolder holder) {
+    public void annotate(String routeName, @NotNull final PsiElement element, @NotNull ProblemsHolder holder) {
 
-        String routeName = element.getContents();
         if(StringUtils.isBlank(routeName)) {
             return;
         }
