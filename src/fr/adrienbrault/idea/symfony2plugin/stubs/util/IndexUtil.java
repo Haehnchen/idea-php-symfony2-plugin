654dc5e222b313ec711e9b0954d92dead9580986
Daniel Espendiller <espendiller@gmx.de>
2014-10-02 17:33:16 +0200

    add settings form button to clean plugin related indexes

---
+++ src/fr/adrienbrault/idea/symfony2plugin/stubs/util/IndexUtil.java
@@ -0,0 +1,31 @@
+package fr.adrienbrault.idea.symfony2plugin.stubs.util;
+
+import com.intellij.util.indexing.FileBasedIndexImpl;
+import com.intellij.util.indexing.ID;
+import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.*;
+
+public class IndexUtil {
+
+    public static void forceReindex() {
+
+        ID<?,?>[] indexIds = new ID<?,?>[] {
+            ServicesDefinitionStubIndex.KEY,
+            AnnotationRoutesStubIndex.KEY,
+            ContainerParameterStubIndex.KEY,
+            ServicesTagStubIndex.KEY,
+            TwigExtendsStubIndex.KEY,
+            TwigIncludeStubIndex.KEY,
+            TwigMacroFromStubIndex.KEY,
+            TwigMacroFunctionStubIndex.KEY,
+            YamlRoutesStubIndex.KEY,
+            YamlTranslationStubIndex.KEY
+        };
+
+        for(ID<?,?> id: indexIds) {
+            FileBasedIndexImpl.getInstance().requestRebuild(id);
+            FileBasedIndexImpl.getInstance().scheduleRebuild(id, new Throwable());
+        }
+
+    }
+
+}
