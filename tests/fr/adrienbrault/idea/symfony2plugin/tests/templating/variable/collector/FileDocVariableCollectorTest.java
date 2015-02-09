d93cd90145303e835fe84d1c421ac3d5984161ae
Daniel Espendiller <espendiller@gmx.de>
2015-02-09 20:32:44 +0100

    add some twig phptypes testing #439

---
+++ tests/fr/adrienbrault/idea/symfony2plugin/tests/templating/variable/collector/FileDocVariableCollectorTest.java
@@ -0,0 +1,58 @@
+package fr.adrienbrault.idea.symfony2plugin.tests.templating.variable.collector;
+
+import com.jetbrains.twig.TwigFileType;
+import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
+
+/**
+ * @author Daniel Espendiller <daniel@espendiller.net>
+ * @see fr.adrienbrault.idea.symfony2plugin.templating.variable.collector.FileDocVariableCollector
+ * @see fr.adrienbrault.idea.symfony2plugin.templating.util.TwigTypeResolveUtil#findFileVariableDocBlock
+ */
+public class FileDocVariableCollectorTest extends SymfonyLightCodeInsightFixtureTestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        myFixture.configureByText("classes.php", "<?php\n" +
+            "namespace Bar;\n" +
+            "class FooClass {\n" +
+            "  public function getFooBar() {}\n" +
+            "  public function setMyBar() {}\n" +
+            "  public function isMyCar() {}\n" +
+            "  public function cool() {}\n" +
+            "  public function Hot() {}\n" +
+            "  protected function protectedBar() {}\n" +
+            "  private function privateBar() {}\n" +
+            "}"
+        );
+
+    }
+
+    public void testFileBasedVarDocPhpTypes() {
+        assertCompletionContains(TwigFileType.INSTANCE, "{# @var bar \\Bar\\FooClass #} {{ <caret> }}", "bar");
+        assertCompletionContains(TwigFileType.INSTANCE, "{# @var bar \\Bar\\FooClass #} {{ bar.<caret> }}", "fooBar", "myCar", "cool", "Hot");
+
+        assertCompletionNotContains(TwigFileType.INSTANCE, "{# @var bar \\Bar\\FooClass #} {{ bar.<caret> }}", "myBar");
+        assertCompletionNotContains(TwigFileType.INSTANCE, "{# @var bar \\Bar\\FooClass #} {{ bar.<caret> }}", "protectedBar");
+        assertCompletionNotContains(TwigFileType.INSTANCE, "{# @var bar \\Bar\\FooClass #} {{ bar.<caret> }}", "privateBar");
+    }
+
+    public void testFileBasedVarDocPhpTypesAsDeprecated() {
+        // remove on dropped feature
+        assertCompletionContains(TwigFileType.INSTANCE, "{# bar \\Bar\\FooClass #} {{ <caret> }}", "bar");
+    }
+
+    /*
+    @TODO: pattern changes?
+    public void testVarArrayIteration() {
+        assertCompletionContains(TwigFileType.INSTANCE, "" +
+            "{# @var bars \\Bar\\FooClass[] #}\n" +
+            "{% for bar in bars %}\n" +
+            "{{ <caret> }}\n" +
+            "{% endfor %}\n"
+            , "bar"
+        );
+    }
+    */
+
+}
