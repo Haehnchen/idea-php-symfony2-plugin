9270c70255f6e290edf1d3bea427cf6aee8e9af0
Daniel Espendiller <espendiller@gmx.de>
2015-07-03 16:43:16 +0200

    test travis cache

--- travis.sh
+++ travis.sh
@@ -1,56 +1,61 @@
 #!/bin/bash
 
 ideaVersion="14.1.4"
+travisCache=".cache"
 
-if [ ! -d ./idea ]; then
-    # Get our IDEA dependency
-    wget http://download.jetbrains.com/idea/ideaIU-${ideaVersion}.tar.gz
-
-    # Unzip IDEA
-    tar zxf ideaIU-${ideaVersion}.tar.gz
-    rm -rf ideaIU-${ideaVersion}.tar.gz
-
-    # Move the versioned IDEA folder to a known location
-    ideaPath=$(find . -name 'idea-IU*' | head -n 1)
-    mv ${ideaPath} ./idea
+if [ ! -d ${travisCache} ]; then
+    echo "Create cache" ${travisCache} 
+    mkdir ${travisCache}
 fi
 
-if [ ! -d ./plugins ]; then
-    # Download required plugins
-    mkdir plugins
-    #php
-    wget https://plugins.jetbrains.com/files/6610/20075/php-141.1534.zip
+function download {
 
-    unzip -qo php-141.1534.zip -d ./plugins
-    rm -rf php-141.1534.zip
-    #twig
-    wget http://plugins.jetbrains.com/files/7303/17519/twig-139.58.zip
+  url=$1
+  basename=${url##*[/|\\]}
+  cachefile=${travisCache}/${basename}
+  
+  if [ ! -f ${cachefile} ]; then
+      wget $url -P ${travisCache};
+    else
+      echo "Cached file `ls -sh $cachefile` - `date -r $cachefile +'%Y-%m-%d %H:%M:%S'`"
+  fi  
 
-    unzip -qo twig-139.58.zip -d ./plugins
-    rm -rf twig-139.58.zip
+  if [ ! -f ${cachefile} ]; then
+    echo "Failed to download: $url"
+    exit 1
+  fi  
+}
 
-    wget http://plugins.jetbrains.com/files/7320/19208/php-annotation.jar
-    mv php-annotation.jar ./plugins
+# Unzip IDEA
+
+if [ -d ./idea  ]; then
+  rm -rf idea
+  mkdir idea
+  echo "created idea dir"  
 fi
 
-# Run the tests
-if [ "$1" = "-d" ]; then
-    ant -d -f build-test.xml -DIDEA_HOME=./idea
-else
-    ant -f build-test.xml -DIDEA_HOME=./idea
+# Download main idea folder
+download "http://download.jetbrains.com/idea/ideaIU-${ideaVersion}.tar.gz"
+tar zxf ${travisCache}/ideaIU-${ideaVersion}.tar.gz -C .
+
+# Move the versioned IDEA folder to a known location
+ideaPath=$(find . -name 'idea-IU*' | head -n 1)
+mv ${ideaPath} ./idea
+  
+if [ -d ./plugins ]; then
+  rm -rf plugins
+  mkdir plugins
+  echo "created plugin dir"  
 fi
 
-# Was our build successful?
-stat=$?
 
-if [ "${TRAVIS}" != true ]; then
-    ant -f build-test.xml -q clean
+#php
+download "https://plugins.jetbrains.com/files/6610/20075/php-141.1534.zip"
+unzip -qo $travisCache/php-141.1534.zip -d ./plugins
+        
+#twig
+download "http://plugins.jetbrains.com/files/7303/17519/twig-139.58.zip"
+unzip -qo $travisCache/twig-139.58.zip -d ./plugins
 
-    if [ "$1" = "-r" ]; then
-        rm -rf idea
-        rm -rf plugins
-    fi
-fi
-
-# Return the build status
-exit ${stat}
+download "http://plugins.jetbrains.com/files/7320/19208/php-annotation.jar"
+cp $travisCache/php-annotation.jar ./plugins
