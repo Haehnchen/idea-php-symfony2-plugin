15acf043c0c3dfccb898d5077bf737696dbbc766
Daniel Espendiller <espendiller@gmx.de>
2015-08-11 19:22:34 +0200

    support "kernel.event_listener" in method type hint #530

--- src/fr/adrienbrault/idea/symfony2plugin/util/EventSubscriberUtil.java
+++ src/fr/adrienbrault/idea/symfony2plugin/util/EventSubscriberUtil.java
@@ -2,7 +2,6 @@ package fr.adrienbrault.idea.symfony2plugin.util;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.xml.XmlTag;
 import com.jetbrains.php.lang.psi.elements.Method;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import fr.adrienbrault.idea.symfony2plugin.config.EventDispatcherSubscriberUtil;
@@ -12,17 +11,11 @@ import fr.adrienbrault.idea.symfony2plugin.dic.tags.ServiceTagInterface;
 import fr.adrienbrault.idea.symfony2plugin.dic.tags.ServiceTagVisitorInterface;
 import fr.adrienbrault.idea.symfony2plugin.stubs.ServiceIndexUtil;
 import fr.adrienbrault.idea.symfony2plugin.util.dict.ServiceUtil;
-import fr.adrienbrault.idea.symfony2plugin.util.yaml.YamlHelper;
-import fr.adrienbrault.idea.symfony2plugin.util.yaml.visitor.YamlServiceTag;
-import fr.adrienbrault.idea.symfony2plugin.util.yaml.visitor.YamlTagVisitor;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.yaml.psi.YAMLKeyValue;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -48,45 +41,14 @@ public class EventSubscriberUtil {
         }
     }
 
+    /**
+     * @TODO: implement collection with prio
+     */
     @Nullable
-    public static String getTaggedEventMethodParameter(Project project, String eventName) {
+    public static String getTaggedEventMethodParameter(@NotNull Project project, @NotNull String eventName) {
 
-        // strong internal events
         if(ServiceUtil.TAGS.containsKey(eventName)) {
             return ServiceUtil.TAGS.get(eventName);
-        }
-
-        EventsCollector[] collectors = new EventsCollector[] {
-            new XmlEventsCollector(),
-            new YmlEventsCollector(),
-        };
-
-        for (String service : ServiceUtil.getTaggedServices(project, "kernel.event_listener")) {
-
-            for (PsiElement psiElement : ServiceIndexUtil.findServiceDefinitions(project, service)) {
-
-                Collection<String> methods = new HashSet<String>();
-
-                for (EventsCollector collector : collectors) {
-                    methods.addAll(collector.collect(psiElement, eventName));
-                }
-
-                // find a method
-                PhpClass phpClass = ServiceUtil.getServiceClass(project, service);
-                if(phpClass != null) {
-                    for (String methodName : methods) {
-                        Method method = phpClass.findMethodByName(methodName);
-                        if(method != null) {
-                            String methodParameterClassHint = PhpElementsUtil.getMethodParameterTypeHint(method);
-                            if(methodParameterClassHint != null) {
-                                return methodParameterClassHint;
-                            }
-                        }
-                    }
-                }
-
-            }
-
         }
 
         for (EventDispatcherSubscribedEvent event : EventDispatcherSubscriberUtil.getSubscribedEvent(project, eventName)) {
@@ -104,81 +66,42 @@ public class EventSubscriberUtil {
             }
         }
 
+        for (String service : ServiceUtil.getTaggedServices(project, "kernel.event_listener")) {
+            for (PsiElement psiElement : ServiceIndexUtil.findServiceDefinitions(project, service)) {
+                Collection<ServiceTagInterface> serviceTagVisitorArguments = ServiceTagFactory.create(service, psiElement);
+
+                if(serviceTagVisitorArguments == null) {
+                    continue;
+                }
+
+                for (ServiceTagInterface tag : serviceTagVisitorArguments) {
+
+                    if(!eventName.equals(tag.getAttribute("event"))) {
+                        continue;
+                    }
+
+                    String methodName = tag.getAttribute("method");
+                    if(StringUtils.isBlank(methodName)) {
+                        continue;
+                    }
+
+                    // @TODO: collect
+                    PhpClass phpClass = ServiceUtil.getServiceClass(project, tag.getServiceId());
+                    if(phpClass != null) {
+                        Method method = phpClass.findMethodByName(methodName);
+                        if(method != null) {
+                            String methodParameterTypeHint = PhpElementsUtil.getMethodParameterTypeHint(method);
+                            if(methodParameterTypeHint != null) {
+                                return methodParameterTypeHint;
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        }
+
         return null;
     }
-
-    public interface EventsCollector {
-        Collection<String> collect(@NotNull PsiElement psiElement, @NotNull String eventName);
-    }
-
-    /**
-     * TODO: implement abstract "TagVisitor"
-     *
-     * - { name: kernel.event_listener, event: eventName, method: methodName }
-     */
-    private static class YmlEventsCollector implements EventsCollector {
-
-        public Collection<String> collect(@NotNull PsiElement psiElement, @NotNull String eventName) {
-
-            if(!(psiElement instanceof YAMLKeyValue)) {
-                return Collections.emptySet();
-            }
-
-            final Collection<String> methods = new HashSet<String>();
-
-            YamlHelper.visitTagsOnServiceDefinition((YAMLKeyValue) psiElement, new YamlTagVisitor() {
-                @Override
-                public void visit(@NotNull YamlServiceTag args) {
-                    if (!"kernel.event_listener".equals(args.getName())) {
-                        return;
-                    }
-
-                    String methodName = args.getAttribute("method");
-                    if (StringUtils.isBlank(methodName)) {
-                        return;
-                    }
-
-                    methods.add(methodName);
-                }
-
-            });
-
-            return methods;
-        }
-
-    }
-
-    /**
-     * TODO: implement abstract "TagVisitor"
-     *
-     * <tag name="kernel.event_listener" event="event_bar" method="foo" />
-     */
-    private static class XmlEventsCollector implements EventsCollector {
-
-        public Collection<String> collect(@NotNull PsiElement psiElement, @NotNull String eventName) {
-
-            if(!(psiElement instanceof XmlTag)) {
-                return Collections.emptySet();
-            }
-
-            Collection<String> methods = new HashSet<String>();
-            for (XmlTag tag : ((XmlTag) psiElement).findSubTags("tag")) {
-
-                if(!"kernel.event_listener".equals(tag.getAttributeValue("name")) || !eventName.equals(tag.getAttributeValue("event"))) {
-                    continue;
-                }
-
-                String methodName = tag.getAttributeValue("method");
-                if(StringUtils.isBlank(methodName)) {
-                    continue;
-                }
-
-                methods.add(methodName);
-            }
-
-            return methods;
-        }
-
-    }
-
 }
