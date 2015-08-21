64574f5f7388d9bc647c8108fdf4cf5bbe61d477
Daniel Espendiller <espendiller@gmx.de>
2015-08-21 16:51:36 +0200

    use inline fixtures for travis and more independent testing

--- tests/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/TwigExtendsStubIndexTest.java
+++ tests/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/TwigExtendsStubIndexTest.java
@@ -1,9 +1,8 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.stubs.indexes;
 
+import com.jetbrains.twig.TwigFileType;
 import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.TwigExtendsStubIndex;
 import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
-
-import java.io.File;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -13,11 +12,12 @@ public class TwigExtendsStubIndexTest extends SymfonyLightCodeInsightFixtureTest
 
     public void setUp() throws Exception {
         super.setUp();
-        myFixture.copyFileToProject("extends.html.twig");
-    }
 
-    public String getTestDataPath() {
-        return new File(this.getClass().getResource("fixtures").getFile()).getAbsolutePath();
+        myFixture.configureByText(TwigFileType.INSTANCE, "" +
+            "{% extends 'extends.html.twig' %}\n" +
+            "{% extends ajax ? 'extends_statement_0.html.twig' : 'extends_statement_1.html.twig' %}\n" +
+            "{% extends request.ajax ? foo ~ \"extends_statement_2.html.twig\" : \"extends_statement_3.html.twig\" %}\n"
+        );
     }
 
     /**
--- tests/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/TwigIncludeStubIndexTest.java
+++ tests/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/TwigIncludeStubIndexTest.java
@@ -1,9 +1,8 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.stubs.indexes;
 
+import com.jetbrains.twig.TwigFileType;
 import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.TwigIncludeStubIndex;
 import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
-
-import java.io.File;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -13,11 +12,21 @@ public class TwigIncludeStubIndexTest extends SymfonyLightCodeInsightFixtureTest
 
     public void setUp() throws Exception {
         super.setUp();
-        myFixture.copyFileToProject("include.html.twig");
-    }
 
-    public String getTestDataPath() {
-        return new File(this.getClass().getResource("fixtures").getFile()).getAbsolutePath();
+        myFixture.configureByText(TwigFileType.INSTANCE, "" +
+            "{% include 'include_foo_quote.html.twig' %}\n" +
+            "{% include \"include_foo_double_quote.html.twig\" %}\n" +
+            "\n" +
+            "{{ include('include_func_func_quote.html.twig') }}\n" +
+            "{{ include(\"include_func_double_quote.html.twig\") }}\n" +
+            "{{ include      (       'include_func_space.html.twig') }}\n" +
+            "\n" +
+            "{{ source('source_quote.html.twig') }}\n" +
+            "{{ source(\"source_double_quote.html.twig\", ignore_missing = true) }}\n" +
+            "\n" +
+            "{% include ajax ? 'include_statement_0.html.twig' : 'include_statement_0.html.twig' %}\n" +
+            "{% include ['include_array_0.html.twig', 'include_array_1.html.twig'] %}"
+        );
     }
 
     /**
