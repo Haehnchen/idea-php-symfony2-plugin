4bf14b01eb23aa07492c5cfbf43f8ca2273f4b5b
Daniel Espendiller <espendiller@gmx.de>
2015-09-03 20:11:30 +0200

    add api "class constant" conditional testing #541

--- tests/fr/adrienbrault/idea/symfony2plugin/tests/doctrine/ObjectRepositoryTypeProviderTest.java
+++ tests/fr/adrienbrault/idea/symfony2plugin/tests/doctrine/ObjectRepositoryTypeProviderTest.java
@@ -1,5 +1,7 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.doctrine;
 
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.patterns.PlatformPatterns;
 import com.jetbrains.php.lang.PhpFileType;
 import com.jetbrains.php.lang.psi.elements.Method;
@@ -46,11 +48,24 @@ public class ObjectRepositoryTypeProviderTest extends SymfonyLightCodeInsightFix
             PlatformPatterns.psiElement(Method.class).withName("bar")
         );
 
-        assertPhpReferenceSignatureEquals(PhpFileType.INSTANCE,
-            "<?php" +
+    }
+
+    public void testGetRepositoryResolveByRepositoryApiClassConstantCompatibility() {
+
+        // Default api level
+        // PhpStorm9 api: 141.2462
+        String result = "#M#" + '\u0151' + "#M#C\\Doctrine\\Common\\Persistence\\ObjectManager.getRepository" + '\u0180' + "#K#C\\Foo\\Bar.class.bar";
+
+        // Old api issue
+        // PhpStorm8 api: 141.1534
+        if(Integer.parseInt(PluginManager.getPlugin(PluginId.getId("com.jetbrains.php")).getVersion().replace(".", "")) <= 1411534) {
+            result = "#M#" + '\u0151' + "#M#C\\Doctrine\\Common\\Persistence\\ObjectManager.getRepository" + '\u0180' + "#K#C\\Foo\\Bar..bar";
+        }
+
+        assertPhpReferenceSignatureEquals(PhpFileType.INSTANCE, "<?php" +
                 "/** @var \\Doctrine\\Common\\Persistence\\ObjectManager $em */\n" +
                 "$em->getRepository(\\Foo\\Bar::class)->b<caret>ar();",
-            "#M#" + '\u0151' + "#M#C\\Doctrine\\Common\\Persistence\\ObjectManager.getRepository" + '\u0180' + "#K#C\\Foo\\Bar..bar"
+            result
         );
     }
 }
