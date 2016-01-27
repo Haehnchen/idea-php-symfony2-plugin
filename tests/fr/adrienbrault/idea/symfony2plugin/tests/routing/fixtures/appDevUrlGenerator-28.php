7c268d602c7903bd6a681d5070459e503c1fd26f
Daniel Espendiller <espendiller@gmx.de>
2016-01-27 18:36:37 +0100

    add Symfony 2.8 appDevUrlGenerator testing fixture

---
+++ tests/fr/adrienbrault/idea/symfony2plugin/tests/routing/fixtures/appDevUrlGenerator-28.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * appDevUrlGenerator
+ *
+ * This class has been auto-generated
+ * by the Symfony Routing Component.
+ */
+class appDevUrlGenerator extends Symfony\Component\Routing\Generator\UrlGenerator
+{
+    private static $declaredRoutes;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(RequestContext $context, LoggerInterface $logger = null)
+    {
+        $this->context = $context;
+        $this->logger = $logger;
+        if (null === self::$declaredRoutes) {
+            self::$declaredRoutes = array(
+        '_wdt' => array (  0 =>   array (    0 => 'token',  ),  1 =>   array (    '_controller' => 'web_profiler.controller.profiler:toolbarAction',  ),  2 =>   array (  ),  3 =>   array (    0 =>     array (      0 => 'variable',      1 => '/',      2 => '[^/]++',      3 => 'token',    ),    1 =>     array (      0 => 'text',      1 => '/_wdt',    ),  ),  4 =>   array (  ),  5 =>   array (  ),),
+    );
+        }
+    }
+}
