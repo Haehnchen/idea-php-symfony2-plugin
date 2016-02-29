caa4bb51563839d630d429d477818a54e203ff1a
Daniel Espendiller <espendiller@gmx.de>
2016-02-29 19:13:34 +0100

    some implementation finding for remote host handling

--- src/fr/adrienbrault/idea/symfony2plugin/Symfony2ProjectComponent.java
+++ src/fr/adrienbrault/idea/symfony2plugin/Symfony2ProjectComponent.java
@@ -1,17 +1,55 @@
 package fr.adrienbrault.idea.symfony2plugin;
 
+import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.deployment.DeploymentUtil;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.io.StreamUtil;
 import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.impl.http.RemoteContentProvider;
+import com.intellij.openapi.vfs.impl.http.RemoteFileManager;
 import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.remoteServer.ServerType;
+import com.intellij.remoteServer.agent.RemoteAgentManager;
+import com.intellij.remoteServer.configuration.RemoteServer;
+import com.intellij.remoteServer.configuration.RemoteServerListener;
+import com.intellij.remoteServer.configuration.RemoteServersManager;
+import com.intellij.remoteServer.configuration.deployment.DeploymentConfiguration;
+import com.intellij.remoteServer.configuration.deployment.DeploymentConfigurationManager;
+import com.intellij.remoteServer.configuration.deployment.DeploymentSource;
+import com.intellij.remoteServer.impl.configuration.RemoteServerImpl;
+import com.intellij.remoteServer.impl.configuration.RemoteServerListConfigurableProvider;
+import com.intellij.remoteServer.impl.runtime.clientLibrary.ClientLibraryManagerImpl;
+import com.intellij.remoteServer.impl.runtime.ui.RemoteServersViewContributor;
+import com.intellij.remoteServer.runtime.ServerConnection;
+import com.intellij.remoteServer.runtime.ServerConnectionManager;
+import com.intellij.remoteServer.runtime.ServerConnector;
+import com.intellij.remoteServer.runtime.clientLibrary.ClientLibraryDescription;
+import com.intellij.remoteServer.runtime.clientLibrary.ClientLibraryManager;
+import com.intellij.remoteServer.runtime.deployment.DeploymentTask;
+import com.intellij.remoteServer.runtime.deployment.ServerRuntimeInstance;
+import com.intellij.remoteServer.runtime.ui.RemoteServersView;
+import com.intellij.util.MessageBusUtil;
+import com.intellij.util.messages.MessageBusConnection;
+import com.intellij.util.messages.impl.MessageBusConnectionImpl;
+import com.jetbrains.plugins.webDeployment.ConnectionOwnerFactory;
+import com.jetbrains.plugins.webDeployment.actions.WebDeploymentDataKeys;
+import com.jetbrains.plugins.webDeployment.config.FileTransferConfig;
+import com.jetbrains.plugins.webDeployment.config.WebServerConfig;
+import com.jetbrains.plugins.webDeployment.config.WebServersConfigManager;
+import com.jetbrains.plugins.webDeployment.connections.RemoteConnection;
+import com.jetbrains.plugins.webDeployment.connections.RemoteConnectionManager;
 import fr.adrienbrault.idea.symfony2plugin.dic.ContainerFile;
 import fr.adrienbrault.idea.symfony2plugin.extension.ServiceContainerLoader;
 import fr.adrienbrault.idea.symfony2plugin.extension.ServiceContainerLoaderParameter;
@@ -19,10 +57,17 @@ import fr.adrienbrault.idea.symfony2plugin.routing.RouteHelper;
 import fr.adrienbrault.idea.symfony2plugin.util.IdeHelper;
 import fr.adrienbrault.idea.symfony2plugin.util.service.ServiceXmlParserFactory;
 import fr.adrienbrault.idea.symfony2plugin.widget.SymfonyProfilerWidget;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.impl.DefaultFileSystemConfigBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.builtInWebServer.WebServerPathHandler;
+import org.jetbrains.builtInWebServer.WebServerPathToFileManager;
+import org.jetbrains.builtInWebServer.WebServerRootsProvider;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -54,6 +99,24 @@ public class Symfony2ProjectComponent implements ProjectComponent {
     }
 
     public void projectOpened() {
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                ApplicationManager.getApplication().invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        foo();
+                    }
+                });
+            }
+        }, 10000);
+
+
+
+
+
+
         this.checkProject();
 
         // phpstorm pre 7.1 dont support statusbar api;
@@ -77,6 +140,125 @@ public class Symfony2ProjectComponent implements ProjectComponent {
             statusBar.addWidget(symfonyProfilerWidget);
         }
 
+
+    }
+
+    private void foo() {
+        System.out.println("1111aaaaa");
+
+        // RemoteFilePanel
+        //RemoteContentProvider.DownloadingCallback
+        project.getMessageBus().connect().subscribe(RemoteServerListener.TOPIC, new RemoteServerListener() {
+            @Override
+            public void serverAdded(@NotNull RemoteServer<?> server) {
+                System.out.println(server);
+            }
+
+            @Override
+            public void serverRemoved(@NotNull RemoteServer<?> server) {
+                System.out.println(server);
+            }
+        });
+
+
+
+
+        for (WebServerConfig webServerConfig : WebServersConfigManager.getInstance(project).getServers(false)) {
+            RemoteConnection foo = null;
+            try {
+                foo = RemoteConnectionManager.getInstance().openConnection(ConnectionOwnerFactory.createConnectionOwner(project), "foo", webServerConfig, FileTransferConfig.Origin.Default, null, null);
+                FileObject file = webServerConfig.findFile(foo.getFileSystem(), new WebServerConfig.RemotePath("composer.json"));
+                System.out.println(StreamUtil.readText(file.getContent().getInputStream(), "UTF-8"));
+            } catch (FileSystemException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+                /*
+        //WebServerPathToFileManager.getInstance().findVirtualFile()
+        for (ServerType serverType : ServerType.EP_NAME.getExtensions()) {
+            for (Object Foo : RemoteServersManager.getInstance().getServers(serverType)) {
+                System.out.println(Foo);
+            }
+        }
+
+        for (ServerType serverType : ServerType.EP_NAME.getExtensions()) {
+           // System.out.println(serverType.getId());
+         //   System.out.println(serverType.getPresentableName());
+        }
+
+        //DeploymentConfigurationManager.getInstance(project).getDeploymentConfigurations(ServerType)
+
+        for (RemoteServersViewContributor contributor : RemoteServersViewContributor.EP_NAME.getExtensions()) {
+            contributor.
+        }
+        */
+
+        //ClientLibraryManagerImpl.getInstance().download(new ClientLibraryDescription());
+        /*
+        for (RemoteServersViewContributor contributor : RemoteServersViewContributor.EP_NAME.getExtensions()) {
+
+            System.out.println(contributor);
+        }
+
+        for (RemoteServer<?> remoteServer : RemoteServersManager.getInstance().getServers()) {
+            System.out.println(remoteServer);
+        }
+
+
+        for (ServerConnection serverConnection : ServerConnectionManager.getInstance().getConnections()) {
+            System.out.println(serverConnection);
+        }
+
+
+        RemoteAgentManager.getInstance().
+
+            ServerConnectionManager.getInstance().getConnection().deploy(new DeploymentTask() {
+            @NotNull
+            @Override
+            public DeploymentSource getSource() {
+                return null;
+            }
+
+            @NotNull
+            @Override
+            public DeploymentConfiguration getConfiguration() {
+                return null;
+            }
+
+            @NotNull
+            @Override
+            public Project getProject() {
+                return null;
+            }
+
+            @Override
+            public boolean isDebugMode() {
+                return false;
+            }
+
+            @NotNull
+            @Override
+            public ExecutionEnvironment getExecutionEnvironment() {
+                return null;
+            }
+        });
+        ServerConnectionManager.getInstance().getConnection().connectIfNeeded(new ServerConnector.ConnectionCallback() {
+            @Override
+            public void connected(@NotNull ServerRuntimeInstance serverRuntimeInstance) {
+                serverRuntimeInstance.depl
+            }
+
+            @Override
+            public void errorOccurred(@NotNull String s) {
+
+            }
+        });
+        */
     }
 
     public void projectClosed() {
