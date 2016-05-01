76fbc08e84d91aa82bc702f395df1982c252e685
Daniel Espendiller <espendiller@gmx.de>
2016-05-01 09:30:22 +0200

    fix exception Accessing indexes from PhpTypeProvider2 while building indexes violates contract #670

--- src/fr/adrienbrault/idea/symfony2plugin/doctrine/metadata/util/DoctrineMetadataUtil.java
+++ src/fr/adrienbrault/idea/symfony2plugin/doctrine/metadata/util/DoctrineMetadataUtil.java
@@ -3,7 +3,6 @@ package fr.adrienbrault.idea.symfony2plugin.doctrine.metadata.util;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.lang.xml.XMLLanguage;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -13,7 +12,6 @@ import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.*;
 import com.intellij.psi.xml.XmlTag;
-import com.intellij.util.Processor;
 import com.intellij.util.indexing.FileBasedIndex;
 import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
@@ -25,9 +23,7 @@ import fr.adrienbrault.idea.symfony2plugin.doctrine.metadata.lookup.DoctrineRepo
 import fr.adrienbrault.idea.symfony2plugin.stubs.cache.FileIndexCaches;
 import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.DoctrineMetadataFileStubIndex;
 import fr.adrienbrault.idea.symfony2plugin.util.PhpElementsUtil;
-import fr.adrienbrault.idea.symfony2plugin.util.PsiElementUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.xmlbeans.XmlLanguage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.yaml.psi.*;
@@ -41,7 +37,7 @@ import java.util.regex.Pattern;
  */
 public class DoctrineMetadataUtil {
 
-    private static final Key<CachedValue<Set<String>>> CLASS_KEYS = new Key<CachedValue<Set<String>>>("CLASS_KEYS");
+    private static final Key<CachedValue<Set<String>>> CLASS_KEYS = new Key<>("CLASS_KEYS");
 
     private static DoctrineMappingDriverInterface[] MAPPING_DRIVERS = new DoctrineMappingDriverInterface[] {
         new DoctrineXmlMappingDriver(),
@@ -51,7 +47,7 @@ public class DoctrineMetadataUtil {
 
     @NotNull
     public static Collection<LookupElement> getObjectRepositoryLookupElements(@NotNull Project project) {
-        return new ArrayList<LookupElement>(DoctrineRepositoryLookupElement.create(PhpIndex.getInstance(project).getAllSubclasses("\\Doctrine\\Common\\Persistence\\ObjectRepository")));
+        return new ArrayList<>(DoctrineRepositoryLookupElement.create(PhpIndex.getInstance(project).getAllSubclasses("\\Doctrine\\Common\\Persistence\\ObjectRepository")));
     }
 
     /**
@@ -60,90 +56,81 @@ public class DoctrineMetadataUtil {
     @Nullable
     public static PhpClass getClassRepository(final @NotNull Project project, final @NotNull String className) {
 
-        final PhpClass[] phpClass = {null};
         for (VirtualFile virtualFile : FileBasedIndex.getInstance().getContainingFiles(DoctrineMetadataFileStubIndex.KEY, className, GlobalSearchScope.allScope(project))) {
 
-            FileBasedIndex.getInstance().processValues(DoctrineMetadataFileStubIndex.KEY, className, virtualFile, new FileBasedIndex.ValueProcessor<DoctrineModelInterface>() {
-                @Override
-                public boolean process(VirtualFile virtualFile, DoctrineModelInterface model) {
-                    if (model == null || model.getRepositoryClass() == null) {
-                        return true;
-                    }
+            final String[] phpClass = {null};
 
-                    phpClass[0] = PhpElementsUtil.getClassInsideNamespaceScope(project, className, model.getRepositoryClass());
+            FileBasedIndex.getInstance().processValues(DoctrineMetadataFileStubIndex.KEY, className, virtualFile, (virtualFile1, model) -> {
+                if (phpClass[0] != null  || model == null || model.getRepositoryClass() == null) {
                     return true;
                 }
+
+                // piping value out of this index thread
+                phpClass[0] = model.getRepositoryClass();
+
+                return true;
             }, GlobalSearchScope.allScope(project));
 
             if(phpClass[0] != null) {
-                return phpClass[0];
+                return PhpElementsUtil.getClassInsideNamespaceScope(project, className, phpClass[0]);
             }
         }
 
-        return phpClass[0];
+        return null;
     }
 
     @NotNull
     public static Collection<VirtualFile> findMetadataFiles(@NotNull Project project, @NotNull String className) {
 
-        final Collection<VirtualFile> virtualFiles = new ArrayList<VirtualFile>();
+        final Collection<VirtualFile> virtualFiles = new ArrayList<>();
 
-        FileBasedIndex.getInstance().getFilesWithKey(DoctrineMetadataFileStubIndex.KEY, new HashSet<String>(Collections.singletonList(className)), new Processor<VirtualFile>() {
-            @Override
-            public boolean process(VirtualFile virtualFile) {
-                virtualFiles.add(virtualFile);
-                return true;
-            }
+        FileBasedIndex.getInstance().getFilesWithKey(DoctrineMetadataFileStubIndex.KEY, new HashSet<>(Collections.singletonList(className)), virtualFile -> {
+            virtualFiles.add(virtualFile);
+            return true;
         }, GlobalSearchScope.allScope(project));
 
         return virtualFiles;
     }
     @NotNull
     public static Collection<VirtualFile> findMetadataForRepositoryClass(@NotNull PhpClass phpClass) {
-        String presentableFQN = phpClass.getPresentableFQN();
-        if(presentableFQN == null) {
-            return Collections.emptyList();
-        }
-
-        if(presentableFQN.startsWith("\\")) {
-            presentableFQN = presentableFQN.substring(1);
-        }
-
-        return findMetadataForRepositoryClass(phpClass.getProject(), presentableFQN);
+        return findMetadataForRepositoryClass(
+            phpClass.getProject(),
+            StringUtils.stripStart(phpClass.getPresentableFQN(), "\\")
+        );
     }
 
-    private static final Key<CachedValue<Map<String, Collection<String>>>> DOCTRINE_REPOSITORY_CACHE = new Key<CachedValue<Map<String, Collection<String>>>>("DOCTRINE_REPOSITORY_CACHE");
+    private static final Key<CachedValue<Map<String, Collection<String>>>> DOCTRINE_REPOSITORY_CACHE;
+
+    static {
+        DOCTRINE_REPOSITORY_CACHE = new Key<>("DOCTRINE_REPOSITORY_CACHE");
+    }
 
     @NotNull
     public static Collection<VirtualFile> findMetadataForRepositoryClass(final @NotNull Project project, @NotNull String repositoryClass) {
 
         CachedValue<Map<String, Collection<String>>> cache = project.getUserData(DOCTRINE_REPOSITORY_CACHE);
         if(cache == null) {
-            cache = CachedValuesManager.getManager(project).createCachedValue(new CachedValueProvider<Map<String, Collection<String>>>() {
-                @Nullable
-                @Override
-                public Result<Map<String, Collection<String>>> compute() {
-                    Map<String, Collection<String>> repositoryMap = new HashMap<String, Collection<String>>();
-                    for (String key : FileIndexCaches.getIndexKeysCache(project, CLASS_KEYS, DoctrineMetadataFileStubIndex.KEY)) {
-                        for (DoctrineModelInterface repositoryDefinition : FileBasedIndex.getInstance().getValues(DoctrineMetadataFileStubIndex.KEY, key, GlobalSearchScope.allScope(project))) {
-                            if(StringUtils.isBlank(repositoryDefinition.getRepositoryClass())) {
-                                continue;
+            cache = CachedValuesManager.getManager(project).createCachedValue(() -> {
+                Map<String, Collection<String>> repositoryMap = new HashMap<>();
+                for (String key : FileIndexCaches.getIndexKeysCache(project, CLASS_KEYS, DoctrineMetadataFileStubIndex.KEY)) {
+                    for (DoctrineModelInterface repositoryDefinition : FileBasedIndex.getInstance().getValues(DoctrineMetadataFileStubIndex.KEY, key, GlobalSearchScope.allScope(project))) {
+                        if(StringUtils.isBlank(repositoryDefinition.getRepositoryClass())) {
+                            continue;
+                        }
+
+                        PhpClass phpClass = PhpElementsUtil.getClassInsideNamespaceScope(project, key, repositoryDefinition.getRepositoryClass());
+                        if(phpClass != null) {
+                            String presentableFQN = phpClass.getPresentableFQN();
+                            if(!repositoryMap.containsKey(presentableFQN)) {
+                                repositoryMap.put(presentableFQN, new HashSet<>());
                             }
 
-                            PhpClass phpClass = PhpElementsUtil.getClassInsideNamespaceScope(project, key, repositoryDefinition.getRepositoryClass());
-                            if(phpClass != null && phpClass.getPresentableFQN() != null) {
-                                String presentableFQN = phpClass.getPresentableFQN();
-                                if(!repositoryMap.containsKey(presentableFQN)) {
-                                    repositoryMap.put(presentableFQN, new HashSet<String>());
-                                }
-
-                                repositoryMap.get(presentableFQN).add(key);
-                            }
+                            repositoryMap.get(presentableFQN).add(key);
                         }
                     }
-
-                    return Result.create(repositoryMap, PsiModificationTracker.MODIFICATION_COUNT);
                 }
+
+                return CachedValueProvider.Result.create(repositoryMap, PsiModificationTracker.MODIFICATION_COUNT);
             }, false);
 
             project.putUserData(DOCTRINE_REPOSITORY_CACHE, cache);
@@ -190,7 +177,7 @@ public class DoctrineMetadataUtil {
                     }
 
                     // @TODO: add target
-                    pair.add(new Pair<String, PsiElement>(table, psiFile));
+                    pair.add(new Pair<>(table, psiFile));
                 }
             }
         }
@@ -251,7 +238,7 @@ public class DoctrineMetadataUtil {
     @NotNull
     public static Collection<PhpClass> getModels(@NotNull Project project) {
 
-        Collection<PhpClass> phpClasses = new ArrayList<PhpClass>();
+        Collection<PhpClass> phpClasses = new ArrayList<>();
         for (String key : FileIndexCaches.getIndexKeysCache(project, CLASS_KEYS, DoctrineMetadataFileStubIndex.KEY)) {
             PhpClass classInterface = PhpElementsUtil.getClassInterface(project, key);
             if(classInterface != null) {
@@ -282,16 +269,13 @@ public class DoctrineMetadataUtil {
     public static String findModelNameInScope(@NotNull PsiElement psiElement) {
 
         if(psiElement.getLanguage().equals(XMLLanguage.INSTANCE)) {
-            PsiElement firstParent = PsiTreeUtil.findFirstParent(psiElement, new Condition<PsiElement>() {
-                @Override
-                public boolean value(PsiElement psiElement) {
-                    if (!(psiElement instanceof XmlTag)) {
-                        return false;
-                    }
-
-                    String name = ((XmlTag) psiElement).getName();
-                    return name.equals("entity") || name.equals("document") || name.equals("embedded") || name.equals("embedded-document");
+            PsiElement firstParent = PsiTreeUtil.findFirstParent(psiElement, psiElement1 -> {
+                if (!(psiElement1 instanceof XmlTag)) {
+                    return false;
                 }
+
+                String name = ((XmlTag) psiElement1).getName();
+                return name.equals("entity") || name.equals("document") || name.equals("embedded") || name.equals("embedded-document");
             });
 
             if(firstParent instanceof XmlTag) {
@@ -324,7 +308,7 @@ public class DoctrineMetadataUtil {
 
     @NotNull
     public static Collection<PhpClass> getClassInsideScope(@NotNull PsiElement psiElement, @NotNull String originValue) {
-        Collection<PhpClass> classesInterface = new ArrayList<PhpClass>();
+        Collection<PhpClass> classesInterface = new ArrayList<>();
         String modelNameInScope = DoctrineMetadataUtil.findModelNameInScope(psiElement);
         if(modelNameInScope != null) {
             PhpClass classInsideNamespaceScope = PhpElementsUtil.getClassInsideNamespaceScope(psiElement.getProject(), modelNameInScope, originValue);
