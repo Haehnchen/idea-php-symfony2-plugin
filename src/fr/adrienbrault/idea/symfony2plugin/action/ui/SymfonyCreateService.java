d09639dcf5684074f7adad32a7976a5a324f0a63
Daniel Espendiller <espendiller@gmx.de>
2016-06-25 12:08:00 +0200

    service generator should close on escape key event

--- src/fr/adrienbrault/idea/symfony2plugin/action/ui/SymfonyCreateService.java
+++ src/fr/adrienbrault/idea/symfony2plugin/action/ui/SymfonyCreateService.java
@@ -46,6 +46,7 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.*;
 import java.util.List;
@@ -224,6 +225,11 @@ public class SymfonyCreateService extends JDialog {
                 generateServiceDefinition();
             }
         });
+
+        // exit on "esc" key
+        this.getRootPane().registerKeyboardAction(e -> {
+            dispose();
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
 
         // insert
         if(this.psiFile instanceof XmlFile || this.psiFile instanceof YAMLFile) {
