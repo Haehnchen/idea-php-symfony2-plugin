d45dcba8eaf5e82112a4ee90caa703de019f8123
Daniel Espendiller <daniel@espendiller.net>
2016-07-06 15:06:22 +0200

    Autocomplete service ids for ContainerBuilder::removeDefinition #761

--- src/fr/adrienbrault/idea/symfony2plugin/config/php/PhpConfigReferenceContributor.java
+++ src/fr/adrienbrault/idea/symfony2plugin/config/php/PhpConfigReferenceContributor.java
@@ -36,6 +36,7 @@ public class PhpConfigReferenceContributor extends PsiReferenceContributor {
             .addCall("\\Symfony\\Component\\DependencyInjection\\ContainerBuilder", "getDefinition")
             .addCall("\\Symfony\\Component\\DependencyInjection\\ContainerBuilder", "setAlias", 1)
             .addCall("\\Symfony\\Component\\DependencyInjection\\ContainerBuilder", "findDefinition")
+            .addCall("\\Symfony\\Component\\DependencyInjection\\ContainerBuilder", "removeDefinition")
         );
 
         psiReferenceRegistrar.registerReferenceProvider(PhpElementsUtil.methodWithFirstStringPattern(), new PhpStringLiteralExpressionReference(TagReference.class)
