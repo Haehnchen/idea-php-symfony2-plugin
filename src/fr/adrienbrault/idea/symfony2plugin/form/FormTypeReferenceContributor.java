83b9ab513309bbe7a33189cb94b4d4e7cb52dbec
Daniel Espendiller <espendiller@gmx.de>
2016-11-10 09:03:15 +0100

    add form placeholder options to translatable value

--- src/fr/adrienbrault/idea/symfony2plugin/form/FormTypeReferenceContributor.java
+++ src/fr/adrienbrault/idea/symfony2plugin/form/FormTypeReferenceContributor.java
@@ -80,7 +80,7 @@ public class FormTypeReferenceContributor extends PsiReferenceContributor {
                         String keyString = key.getContents();
 
                         // @TODO: how to handle custom bundle fields like help_block
-                        if(keyString.equals("label") || keyString.equals("help_block") || keyString.equals("help_inline")) {
+                        if(keyString.equals("label") || keyString.equals("help_block") || keyString.equals("help_inline") || keyString.equals("placeholder")) {
 
                             // translation_domain in current array block
                             String translationDomain = PhpElementsUtil.getArrayHashValue(arrayCreation, "translation_domain");
