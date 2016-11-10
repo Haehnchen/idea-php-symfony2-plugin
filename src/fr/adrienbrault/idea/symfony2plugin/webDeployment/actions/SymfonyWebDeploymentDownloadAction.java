a111533ee7971315b776c060b71eef05edd6c55b
Shaverdova Elena <Elena.Shaverdova@jetbrains.com>
2016-11-10 19:14:03 +0100

    Make compatible with new Deployment API

--- src/fr/adrienbrault/idea/symfony2plugin/webDeployment/actions/SymfonyWebDeploymentDownloadAction.java
+++ src/fr/adrienbrault/idea/symfony2plugin/webDeployment/actions/SymfonyWebDeploymentDownloadAction.java
@@ -2,14 +2,13 @@ package fr.adrienbrault.idea.symfony2plugin.webDeployment.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.plugins.webDeployment.actions.WebDeploymentDataKeys;
+import com.jetbrains.plugins.webDeployment.config.Deployable;
 import com.jetbrains.plugins.webDeployment.config.PublishConfig;
-import com.jetbrains.plugins.webDeployment.config.WebServerConfig;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2Icons;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
 import fr.adrienbrault.idea.symfony2plugin.webDeployment.utils.RemoteWebServerUtil;
@@ -28,7 +27,7 @@ public class SymfonyWebDeploymentDownloadAction extends DumbAwareAction {
             return;
         }
 
-        WebServerConfig server = WebDeploymentDataKeys.SERVER_CONFIG.getData(e.getDataContext());
+        Deployable server = WebDeploymentDataKeys.DEPLOYABLE.getData(e.getDataContext());
         if(server == null || !PublishConfig.getInstance(project).isDefault(server) || !server.needsTransfer() || server.validateFast() != null) {
             e.getPresentation().setVisible(false);
             return;
@@ -43,7 +42,7 @@ public class SymfonyWebDeploymentDownloadAction extends DumbAwareAction {
             return;
         }
 
-        WebServerConfig server = WebDeploymentDataKeys.SERVER_CONFIG.getData(e.getDataContext());
+        Deployable server = WebDeploymentDataKeys.DEPLOYABLE.getData(e.getDataContext());
         if(server == null || !PublishConfig.getInstance(project).isDefault(server)) {
             return;
         }
