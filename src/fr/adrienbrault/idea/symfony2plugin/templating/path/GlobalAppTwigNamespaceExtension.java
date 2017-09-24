13e1285e7ecbc04a140b3b994320261cfd955c69
Daniel Espendiller <espendiller@gmx.de>
2017-09-24 11:57:56 +0200

    add support for Symfony flex template path structure #922

--- src/fr/adrienbrault/idea/symfony2plugin/templating/path/GlobalAppTwigNamespaceExtension.java
+++ src/fr/adrienbrault/idea/symfony2plugin/templating/path/GlobalAppTwigNamespaceExtension.java
@@ -7,13 +7,17 @@ import fr.adrienbrault.idea.symfony2plugin.extension.TwigNamespaceExtension;
 import fr.adrienbrault.idea.symfony2plugin.extension.TwigNamespaceExtensionParameter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 
 /**
  * app/Resources/views/foo.html.twig => :foo.html.twig
  * app/Resources/views/foo.html.twig => foo.html.twig
+ *
+ * /templates/foo.html.twig => foo.html.twig
+ * /templates/foo.html.twig => :foo.html.twig
  *
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
@@ -21,19 +25,28 @@ public class GlobalAppTwigNamespaceExtension implements TwigNamespaceExtension {
     @NotNull
     @Override
     public Collection<TwigPath> getNamespaces(@NotNull TwigNamespaceExtensionParameter parameter) {
-        String appDirectoryName = Settings.getInstance(parameter.getProject()).directoryToApp + "/Resources/views";
         VirtualFile baseDir = parameter.getProject().getBaseDir();
 
-        VirtualFile globalDirectory = VfsUtil.findRelativeFile(baseDir, appDirectoryName.split("/"));
-        if(globalDirectory == null) {
-            return Collections.emptyList();
+        Collection<TwigPath> paths = new ArrayList<>();
+
+        List<String[]> templatePaths = Arrays.asList(
+            new String[]{Settings.getInstance(parameter.getProject()).directoryToApp, "Resources", "views"},
+            new String[]{"templates"}
+        );
+
+        for (String[] strings1 : templatePaths) {
+            VirtualFile globalDirectory = VfsUtil.findRelativeFile(baseDir, strings1);
+
+            if(globalDirectory == null) {
+                continue;
+            }
+
+            String path = globalDirectory.getPath();
+
+            paths.add(new TwigPath(path, TwigPathIndex.MAIN, TwigPathIndex.NamespaceType.BUNDLE));
+            paths.add(new TwigPath(path, TwigPathIndex.MAIN, TwigPathIndex.NamespaceType.ADD_PATH));
         }
 
-        String path = globalDirectory.getPath();
-
-        return Arrays.asList(
-            new TwigPath(path, TwigPathIndex.MAIN, TwigPathIndex.NamespaceType.BUNDLE),
-            new TwigPath(path, TwigPathIndex.MAIN, TwigPathIndex.NamespaceType.ADD_PATH)
-        );
+        return paths;
     }
 }
