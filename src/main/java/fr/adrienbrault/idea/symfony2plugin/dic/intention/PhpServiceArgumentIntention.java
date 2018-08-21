243d9896900b989f2793edf7ea0e08952d4ee056
Cedric Ziel <cedric@cedric-ziel.com>
2018-08-21 10:46:28 +0200

    Move write action out of AWT event for PhpServiceArgumentIntention
    Fixes: #1184

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/dic/intention/PhpServiceArgumentIntention.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/dic/intention/PhpServiceArgumentIntention.java
@@ -10,7 +10,7 @@ import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlTag;
-import com.intellij.ui.components.JBList;
+import com.intellij.util.Consumer;
 import com.intellij.util.IncorrectOperationException;
 import com.jetbrains.php.lang.PhpLanguage;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
@@ -30,57 +30,63 @@ public class PhpServiceArgumentIntention extends PsiElementBaseIntentionAction {
     @Override
     public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement psiElement) throws IncorrectOperationException {
         PhpClass phpClass = PsiTreeUtil.getParentOfType(psiElement, PhpClass.class);
-        if(phpClass != null) {
-            Set<String> serviceNames = ContainerCollectionResolver.ServiceCollector.create(project).convertClassNameToServices(phpClass.getFQN());
-            if(serviceNames.size() > 0) {
-                Collection<PsiElement> psiElements = new ArrayList<>();
-                for(String serviceName: serviceNames) {
-                    psiElements.addAll(ServiceIndexUtil.findServiceDefinitions(project, serviceName));
-                }
+        if (phpClass == null) {
 
-                if(psiElements.size() > 0) {
-                    Map<String, PsiElement> map = new HashMap<>();
-
-                    for (PsiElement element : psiElements) {
-                        map.put(VfsUtil.getRelativePath(element.getContainingFile().getVirtualFile(), element.getProject().getBaseDir()), element);
-                    }
-
-                    final JBList<String> list = new JBList<>(map.keySet());
-
-                    JBPopupFactory.getInstance().createListPopupBuilder(list)
-                        .setTitle("Symfony: Services Definitions")
-                        .setItemChoosenCallback(() -> new WriteCommandAction.Simple(editor.getProject(), "Service Update") {
-                            @Override
-                            protected void run() {
-                                String selectedValue = list.getSelectedValue();
-
-                                PsiElement target = map.get(selectedValue);
-                                invokeByScope(target, editor);
-                            }
-                        }.execute())
-                        .createPopup()
-                        .showInBestPositionFor(editor);
-                }
-            }
+            return;
         }
+
+        Set<String> serviceNames = ContainerCollectionResolver.ServiceCollector.create(project).convertClassNameToServices(phpClass.getFQN());
+        if (serviceNames.isEmpty()) {
+
+            return;
+        }
+
+        Collection<PsiElement> psiElements = new ArrayList<>();
+        for (String serviceName : serviceNames) {
+            psiElements.addAll(ServiceIndexUtil.findServiceDefinitions(project, serviceName));
+        }
+
+        if (psiElements.isEmpty()) {
+
+            return;
+        }
+
+        Map<String, PsiElement> map = new HashMap<>();
+
+        for (PsiElement element : psiElements) {
+            map.put(VfsUtil.getRelativePath(element.getContainingFile().getVirtualFile(), element.getProject().getBaseDir()), element);
+        }
+
+        Consumer<String> consumer = (selected) -> {
+            WriteCommandAction.writeCommandAction(project).withName("Service Update").run(() -> {
+                invokeByScope(map.get(selected), editor);
+            });
+        };
+
+        JBPopupFactory.getInstance().createPopupChooserBuilder(new ArrayList<>(map.keySet()))
+                .setTitle("Symfony: Services Definitions")
+                .setItemChosenCallback(consumer)
+                .createPopup()
+                .showInBestPositionFor(editor)
+        ;
     }
 
     private void invokeByScope(@NotNull PsiElement psiElement, @NotNull Editor editor) {
         boolean success = false;
 
-        if(psiElement instanceof XmlTag) {
+        if (psiElement instanceof XmlTag) {
             List<String> args = ServiceActionUtil.getXmlMissingArgumentTypes((XmlTag) psiElement, true, new ContainerCollectionResolver.LazyServiceCollector(psiElement.getProject()));
 
             success = args.size() > 0;
             if (success) {
                 ServiceActionUtil.fixServiceArgument(args, (XmlTag) psiElement);
             }
-        } else if(psiElement instanceof YAMLKeyValue) {
+        } else if (psiElement instanceof YAMLKeyValue) {
             List<String> args = ServiceActionUtil.getYamlMissingArgumentTypes(
-                psiElement.getProject(),
-                ServiceActionUtil.ServiceYamlContainer.create((YAMLKeyValue) psiElement),
-                false,
-                new ContainerCollectionResolver.LazyServiceCollector(psiElement.getProject())
+                    psiElement.getProject(),
+                    ServiceActionUtil.ServiceYamlContainer.create((YAMLKeyValue) psiElement),
+                    false,
+                    new ContainerCollectionResolver.LazyServiceCollector(psiElement.getProject())
             );
 
             success = args.size() > 0;
@@ -89,13 +95,14 @@ public class PhpServiceArgumentIntention extends PsiElementBaseIntentionAction {
             }
         }
 
-        if(!success) {
+        if (!success) {
             HintManager.getInstance().showErrorHint(editor, "No argument update needed");
+
             return;
         }
 
         String relativePath = VfsUtil.getRelativePath(psiElement.getContainingFile().getVirtualFile(), psiElement.getProject().getBaseDir());
-        if(relativePath == null) {
+        if (relativePath == null) {
             relativePath = "n/a";
         }
 
@@ -125,7 +132,7 @@ public class PhpServiceArgumentIntention extends PsiElementBaseIntentionAction {
         PhpClass phpClass = PsiTreeUtil.getParentOfType(psiElement, PhpClass.class);
 
         return phpClass == null
-            ? Collections.emptySet()
-            : ContainerCollectionResolver.ServiceCollector.create(psiElement.getProject()).convertClassNameToServices(phpClass.getFQN());
+                ? Collections.emptySet()
+                : ContainerCollectionResolver.ServiceCollector.create(psiElement.getProject()).convertClassNameToServices(phpClass.getFQN());
     }
 }
