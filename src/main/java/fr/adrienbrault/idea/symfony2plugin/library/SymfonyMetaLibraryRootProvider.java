209ee806ce70a590504388b1fde91d43ab415edb
Thomas Schulz <mail@king2500.net>
2019-04-06 01:04:19 +0200

    Change static libraryRoots to dynamic LibraryRootProvider implementations

---
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/library/SymfonyMetaLibraryRootProvider.java
@@ -0,0 +1,19 @@
+package fr.adrienbrault.idea.symfony2plugin.library;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Stream;
+
+public class SymfonyMetaLibraryRootProvider extends PluginLibraryRootProvider {
+    @NotNull
+    @Override
+    public Stream<VirtualFile> getLibraryRoots(@NotNull Project project) {
+        if (!Symfony2ProjectComponent.isEnabled(project)) {
+            return Stream.empty();
+        }
+        return this.getPluginPathAsStream("/symfony-meta/");
+    }
+}
