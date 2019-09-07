07e47ffb2656a0438e28a46919dd4c5ebe4e0dee
Daniel Espendiller <daniel@espendiller.net>
2019-09-07 12:59:05 +0200

    remove custom related template search for controller and migrate to indexer visitor

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/navigation/controller/TemplatesControllerRelatedGotoCollector.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/navigation/controller/TemplatesControllerRelatedGotoCollector.java
@@ -1,100 +1,33 @@
 package fr.adrienbrault.idea.symfony2plugin.navigation.controller;
 
-import com.intellij.openapi.util.Pair;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment;
-import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag;
-import com.jetbrains.php.lang.psi.elements.Method;
-import com.jetbrains.php.lang.psi.elements.PhpClass;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2Icons;
-import fr.adrienbrault.idea.symfony2plugin.config.SymfonyPhpReferenceContributor;
 import fr.adrienbrault.idea.symfony2plugin.dic.RelatedPopupGotoLineMarker;
 import fr.adrienbrault.idea.symfony2plugin.extension.ControllerActionGotoRelatedCollector;
 import fr.adrienbrault.idea.symfony2plugin.extension.ControllerActionGotoRelatedCollectorParameter;
+import fr.adrienbrault.idea.symfony2plugin.templating.util.PhpMethodVariableResolveUtil;
 import fr.adrienbrault.idea.symfony2plugin.templating.util.TwigUtil;
-import fr.adrienbrault.idea.symfony2plugin.util.AnnotationBackportUtil;
-import fr.adrienbrault.idea.symfony2plugin.util.MethodMatcher;
-import fr.adrienbrault.idea.symfony2plugin.util.PhpElementsUtil;
 import icons.TwigIcons;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
-import java.util.function.Consumer;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
 public class TemplatesControllerRelatedGotoCollector implements ControllerActionGotoRelatedCollector {
-
     @Override
     public void collectGotoRelatedItems(ControllerActionGotoRelatedCollectorParameter parameter) {
         Set<String> uniqueTemplates = new HashSet<>();
 
-        Method method = parameter.getMethod();
-
-        visitMethodTemplateNames(method, pair -> {
-            String templateName = pair.getFirst();
-
-            if(!uniqueTemplates.contains(templateName)) {
-                uniqueTemplates.add(templateName);
-
-                for (PsiElement psiElement : pair.getSecond()) {
-                    parameter.add(new RelatedPopupGotoLineMarker.PopupGotoRelatedItem(
-                        psiElement,
-                        TwigUtil.getFoldingTemplateNameOrCurrent(templateName)).withIcon(TwigIcons.TwigFileIcon, Symfony2Icons.TWIG_LINE_MARKER)
-                    );
-                }
-            }
+        PhpMethodVariableResolveUtil.visitRenderTemplateFunctions(parameter.getMethod(), triple -> {
+            uniqueTemplates.add(triple.getFirst());
         });
 
-        for(PsiElement psiElement: parameter.getParameterLists()) {
-            MethodMatcher.MethodMatchParameter matchedSignature = MethodMatcher.getMatchedSignatureWithDepth(psiElement, SymfonyPhpReferenceContributor.TEMPLATE_SIGNATURES);
-            if (matchedSignature != null) {
-                String resolveString = PhpElementsUtil.getStringValue(psiElement);
-                if(resolveString != null && !uniqueTemplates.contains(resolveString)) {
-                    uniqueTemplates.add(resolveString);
-                    for(PsiFile templateTarget: TwigUtil.getTemplatePsiElements(parameter.getProject(), resolveString)) {
-                        parameter.add(new RelatedPopupGotoLineMarker.PopupGotoRelatedItem(templateTarget, resolveString).withIcon(TwigIcons.TwigFileIcon, Symfony2Icons.TWIG_LINE_MARKER));
-                    }
-                }
+        for (String uniqueTemplate : uniqueTemplates) {
+            for(PsiFile templateTarget: TwigUtil.getTemplatePsiElements(parameter.getProject(), uniqueTemplate)) {
+                parameter.add(new RelatedPopupGotoLineMarker.PopupGotoRelatedItem(templateTarget, uniqueTemplate).withIcon(TwigIcons.TwigFileIcon, Symfony2Icons.TWIG_LINE_MARKER));
             }
-        }
-    }
-
-    /**
-     *  Visit every possible template on given method: eg Annotations @Template()
-     */
-    public static void visitMethodTemplateNames(@NotNull Method method, @NotNull Consumer<Pair<String, Collection<PsiElement>>> consumer) {
-        // on @Template annotation
-        PhpDocComment phpDocComment = method.getDocComment();
-        if(phpDocComment != null) {
-            Collection<PhpDocTag> phpDocTags = AnnotationBackportUtil.filterValidDocTags(PsiTreeUtil.findChildrenOfType(phpDocComment, PhpDocTag.class));
-            if(phpDocTags.size() > 0) {
-                // cache use map for this phpDocComment
-                Map<String, String> importMap = AnnotationBackportUtil.getUseImportMap(phpDocComment);
-                if(importMap.size() > 0) {
-                    for(PhpDocTag phpDocTag: phpDocTags) {
-                        // resolve annotation and check for template
-                        PhpClass phpClass = AnnotationBackportUtil.getAnnotationReference(phpDocTag, importMap);
-                        if(phpClass != null && PhpElementsUtil.isEqualClassName(phpClass, TwigUtil.TEMPLATE_ANNOTATION_CLASS)) {
-                            Pair<String, Collection<PsiElement>> templateAnnotationFiles = TwigUtil.getTemplateAnnotationFiles(phpDocTag);
-                            if(templateAnnotationFiles != null) {
-                                consumer.accept(Pair.create(templateAnnotationFiles.getFirst(), templateAnnotationFiles.getSecond()));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // on method name
-        for (String templateName : TwigUtil.getControllerMethodShortcut(method)) {
-            consumer.accept(Pair.create(
-                templateName,
-                new HashSet<>(TwigUtil.getTemplatePsiElements(method.getProject(), templateName))
-            ));
         }
     }
 }
