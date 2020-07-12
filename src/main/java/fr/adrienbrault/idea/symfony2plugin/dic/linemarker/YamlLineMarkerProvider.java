3ea900799b233e65f808c10b0df0c96d73391c07
Daniel Espendiller <daniel@espendiller.net>
2020-07-12 11:26:49 +0200

    provide linemarker for yaml service resource

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/dic/linemarker/YamlLineMarkerProvider.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/dic/linemarker/YamlLineMarkerProvider.java
@@ -3,9 +3,12 @@ package fr.adrienbrault.idea.symfony2plugin.dic.linemarker;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.psi.PsiElement;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
 import fr.adrienbrault.idea.symfony2plugin.config.yaml.YamlElementPatternHelper;
+import fr.adrienbrault.idea.symfony2plugin.config.yaml.YamlGoToDeclarationHandler;
 import fr.adrienbrault.idea.symfony2plugin.util.dict.ServiceUtil;
 import fr.adrienbrault.idea.symfony2plugin.util.yaml.YamlHelper;
 import org.apache.commons.lang.StringUtils;
@@ -52,7 +55,30 @@ public class YamlLineMarkerProvider implements LineMarkerProvider {
 
             // services -> service_name
             visitServiceId(psiElement, (YAMLKeyValue) yamlKeyValue, result, lazyDecoratedServices);
+
+            // services:
+            //    App\:
+            //        resource: '../src/*'
+            visitServiceIdForResources(psiElement, (YAMLKeyValue) yamlKeyValue, result);
         }
+    }
+
+    private void visitServiceIdForResources(PsiElement leafTarget, YAMLKeyValue yamlKeyValue, @NotNull Collection<LineMarkerInfo> result) {
+        String resource = YamlHelper.getYamlKeyValueAsString(yamlKeyValue, "resource");
+        if (resource == null) {
+            return;
+        }
+
+        result.add(NavigationGutterIconBuilder.create(AllIcons.Modules.SourceRoot)
+            .setTargets(new NotNullLazyValue<Collection<? extends PsiElement>>() {
+                @NotNull
+                @Override
+                protected Collection<? extends PsiElement> compute() {
+                    return YamlGoToDeclarationHandler.getClassesForServiceKey(yamlKeyValue);
+                }
+            })
+            .setTooltipText("Navigate to class")
+            .createLineMarkerInfo(leafTarget));
     }
 
     private void visitServiceId(@NotNull PsiElement leafTarget, @NotNull YAMLKeyValue yamlKeyValue, @NotNull Collection<LineMarkerInfo> result, @NotNull LazyDecoratedParentServiceValues lazyDecoratedServices) {
@@ -63,13 +89,13 @@ public class YamlLineMarkerProvider implements LineMarkerProvider {
 
         // decorates: foobar
         String decorates = YamlHelper.getYamlKeyValueAsString(yamlKeyValue, "decorates");
-        if(decorates != null && StringUtils.isNotBlank(decorates)) {
+        if(StringUtils.isNotBlank(decorates)) {
             result.add(ServiceUtil.getLineMarkerForDecoratesServiceId(leafTarget, ServiceUtil.ServiceLineMarker.DECORATE, decorates));
         }
 
         // parent: foobar
         String parent = YamlHelper.getYamlKeyValueAsString(yamlKeyValue, "parent");
-        if(parent != null && StringUtils.isNotBlank(parent)) {
+        if(StringUtils.isNotBlank(parent)) {
             result.add(ServiceUtil.getLineMarkerForDecoratesServiceId(leafTarget, ServiceUtil.ServiceLineMarker.PARENT, parent));
         }
 
