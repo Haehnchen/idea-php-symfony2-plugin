bf0869d51f0cb23f7b15567336a15852673789f7
Daniel Espendiller <daniel@espendiller.net>
2020-08-04 17:25:29 +0200

    WIP: form field navigation and linemarker for Twig

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/templating/variable/resolver/holder/FormDataHolder.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/templating/variable/resolver/holder/FormDataHolder.java
@@ -1,7 +1,9 @@
 package fr.adrienbrault.idea.symfony2plugin.templating.variable.resolver.holder;
 
+import com.intellij.psi.PsiElement;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -13,9 +15,18 @@ public class FormDataHolder {
     @NotNull
     private final PhpClass formType;
 
+    @Nullable
+    private PsiElement field;
+
     public FormDataHolder(@NotNull PhpClass phpClass, @NotNull PhpClass formType) {
         this.phpClass = phpClass;
         this.formType = formType;
+    }
+
+    public FormDataHolder(@NotNull PhpClass phpClass, @NotNull PhpClass formType, @NotNull PsiElement field) {
+        this.phpClass = phpClass;
+        this.formType = formType;
+        this.field = field;
     }
 
     @NotNull
@@ -26,5 +37,10 @@ public class FormDataHolder {
     @NotNull
     public PhpClass getFormType() {
         return formType;
+    }
+
+    @Nullable
+    public PsiElement getField() {
+        return this.field;
     }
 }
