5bd233de26a1ebf3234ad1115537b2321c8b7dbb
Adam Wójs <adam@wojs.pl>
2022-05-01 10:31:59 +0200

    Added service name reference contributor for YAML DIC files

---
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/config/yaml/ServiceYamlReference.java
@@ -0,0 +1,21 @@
+package fr.adrienbrault.idea.symfony2plugin.config.yaml;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import fr.adrienbrault.idea.symfony2plugin.dic.AbstractServiceReference;
+import org.jetbrains.annotations.NotNull;
+
+public class ServiceYamlReference extends AbstractServiceReference {
+
+    public ServiceYamlReference(@NotNull PsiElement psiElement, @NotNull String serviceId) {
+        super(psiElement);
+
+        this.serviceId = serviceId;
+    }
+
+    public ServiceYamlReference(@NotNull PsiElement psiElement, @NotNull TextRange range, @NotNull String serviceId) {
+        super(psiElement, range);
+
+        this.serviceId = serviceId;
+    }
+}
