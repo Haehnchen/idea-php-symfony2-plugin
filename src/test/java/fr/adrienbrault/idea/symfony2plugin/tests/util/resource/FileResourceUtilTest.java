6f0a9c2ebfc2a6677fa3683abcdb6b7e7189ba6f
Daniel Espendiller <daniel@espendiller.net>
2022-05-31 19:23:51 +0200

    provide "glob" pattern extraction for string to provide a file scanner

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/util/resource/FileResourceUtilTest.java
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/util/resource/FileResourceUtilTest.java
@@ -1,11 +1,12 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.util.resource;
 
+import com.intellij.openapi.util.Pair;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.containers.ContainerUtil;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
 import fr.adrienbrault.idea.symfony2plugin.util.resource.FileResourceUtil;
-import kotlin.Pair;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -41,5 +42,27 @@ public class FileResourceUtilTest extends SymfonyLightCodeInsightFixtureTestCase
                 psiElement instanceof PhpClass && "\\FooBundle\\Controller\\FooController".equals(((PhpClass) psiElement).getFQN())
             ));
         }
+    }
+
+    public void testGetGlobalPatternDirectory() {
+        @NotNull Pair<String, String> globalPatternDirectory = FileResourceUtil.getGlobalPatternDirectory("../src/DependencyInjection/");
+        assertEquals("../src/DependencyInjection/", globalPatternDirectory.getFirst());
+        assertNull(globalPatternDirectory.getSecond());
+
+        globalPatternDirectory = FileResourceUtil.getGlobalPatternDirectory("../src/DependencyInjection/**/test.php");
+        assertEquals("../src/DependencyInjection", globalPatternDirectory.getFirst());
+        assertEquals("**/test.php", globalPatternDirectory.getSecond());
+
+        globalPatternDirectory = FileResourceUtil.getGlobalPatternDirectory("../src/{Entity,Foobar}/");
+        assertEquals("../src", globalPatternDirectory.getFirst());
+        assertEquals("{Entity,Foobar}/", globalPatternDirectory.getSecond());
+
+        globalPatternDirectory = FileResourceUtil.getGlobalPatternDirectory("../src/Kernel.php");
+        assertEquals("../src/Kernel.php", globalPatternDirectory.getFirst());
+        assertNull(globalPatternDirectory.getSecond());
+
+        globalPatternDirectory = FileResourceUtil.getGlobalPatternDirectory("src/");
+        assertEquals("src/", globalPatternDirectory.getFirst());
+        assertNull(globalPatternDirectory.getSecond());
     }
 }
