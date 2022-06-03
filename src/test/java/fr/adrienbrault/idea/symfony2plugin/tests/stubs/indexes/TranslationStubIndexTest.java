56a6c885b76b1494c16f4eeed89897653b5ef693
Daniel Espendiller <daniel@espendiller.net>
2022-06-03 19:57:32 +0200

    try to detect yaml translation files also on language pattern

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/TranslationStubIndexTest.java
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/TranslationStubIndexTest.java
@@ -1,14 +1,20 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.stubs.indexes;
 
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.Processor;
 import com.intellij.util.containers.ArrayListSet;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.FileBasedIndex;
+import com.jetbrains.twig.TwigFileType;
 import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.TranslationStubIndex;
 import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -36,10 +42,33 @@ public class TranslationStubIndexTest extends SymfonyLightCodeInsightFixtureTest
 
         myFixture.copyFileToProject("fr.xlf", "+intl-icu.fr.xlf");
         myFixture.copyFileToProject("fr.xlf", "  .fr.xlf");
+
+        myFixture.copyFileToProject("apple.de.yml", "unknown/unknown-1+intl-icu.fr.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/messages.ar.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/validators.ar.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/unknown-2.fr.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/unknown-3.sr_Cyrl.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/unknown-3.sr_Cyrla.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/unknown-3.xx.yml");
+        myFixture.copyFileToProject("apple.de.yml", "unknown/unknown-3.yml");
     }
 
     public String getTestDataPath() {
         return "src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/stubs/indexes/fixtures";
+    }
+
+    public void testNamingMatchingForTranslations() {
+        Set<String> filenames = new HashSet<>();
+
+        for (String key : new String[]{"unknown-1", "unknown-2", "unknown-3", "validators", "messages"}) {
+            FileBasedIndex.getInstance().getFilesWithKey(TranslationStubIndex.KEY, new HashSet<>(List.of(key)), virtualFile -> {
+                filenames.add(virtualFile.getName());
+                return true;
+            }, GlobalSearchScope.allScope(getProject()));
+        }
+
+        assertContainsElements(filenames, "unknown-1+intl-icu.fr.yml", "messages.ar.yml", "validators.ar.yml", "unknown-2.fr.yml", "unknown-3.sr_Cyrl.yml");
+        assertDoesntContain(filenames, "unknown-3.sr_Cyrla.yml", "unknown-3.xx.yml", "unknown-3.yml");
     }
 
     public void testThatDomainFromFileIsExtracted() {
