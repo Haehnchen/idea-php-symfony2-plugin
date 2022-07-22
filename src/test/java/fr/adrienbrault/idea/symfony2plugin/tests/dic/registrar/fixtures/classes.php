a73032f72c9c6b9ad79c847cbb2c15c574c11bea
Daniel Espendiller <daniel@espendiller.net>
2022-07-22 20:01:51 +0200

    #1984 support parameters inside "Autowire" attribute

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/dic/registrar/fixtures/classes.php
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/dic/registrar/fixtures/classes.php
@@ -9,6 +9,23 @@ namespace Symfony\Component\DependencyInjection
     }
 }
 
+namespace Symfony\Component\DependencyInjection\Attribute
+{
+    class Autowire
+    {
+        /**
+         * @param string|null $value      Parameter value (ie "%kernel.project_dir%/some/path")
+         * @param string|null $service    Service ID (ie "some.service")
+         * @param string|null $expression Expression (ie 'service("some.service").someMethod()')
+         */
+        public function __construct(
+            string $value = null,
+            string $service = null,
+            string $expression = null,
+        ) {}
+    }
+}
+
 namespace
 {
     class Foo
