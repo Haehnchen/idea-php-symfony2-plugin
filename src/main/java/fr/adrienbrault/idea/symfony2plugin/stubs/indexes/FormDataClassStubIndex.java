f557e92bedf6386a5d4137e56b26117326ab2f8a
Daniel Espendiller <daniel@espendiller.net>
2023-03-24 17:25:12 +0100

    migrate form data class indexer to use control flow

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/stubs/indexes/FormDataClassStubIndex.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/stubs/indexes/FormDataClassStubIndex.java
@@ -2,22 +2,24 @@ package fr.adrienbrault.idea.symfony2plugin.stubs.indexes;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiRecursiveElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.indexing.*;
 import com.intellij.util.io.DataExternalizer;
 import com.intellij.util.io.EnumeratorStringDescriptor;
 import com.intellij.util.io.KeyDescriptor;
+import com.jetbrains.php.codeInsight.controlFlow.PhpControlFlowUtil;
+import com.jetbrains.php.codeInsight.controlFlow.PhpInstructionProcessor;
+import com.jetbrains.php.codeInsight.controlFlow.instructions.PhpCallInstruction;
 import com.jetbrains.php.lang.PhpFileType;
 import com.jetbrains.php.lang.psi.PhpFile;
 import com.jetbrains.php.lang.psi.elements.*;
 import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.externalizer.StringSetDataExternalizer;
 import fr.adrienbrault.idea.symfony2plugin.util.PhpElementsUtil;
-import gnu.trove.THashMap;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -45,87 +47,95 @@ public class FormDataClassStubIndex extends FileBasedIndexExtension<String, Set<
     @Override
     public DataIndexer<String, Set<String>, FileContent> getIndexer() {
         return inputData -> {
-            Map<String, Set<String>> map = new THashMap<>();
+            Map<String, Set<String>> map = new HashMap<>();
 
             PsiFile psiFile = inputData.getPsiFile();
             if(!(psiFile instanceof PhpFile)) {
                 return map;
             }
 
-            psiFile.accept(new PsiRecursiveElementVisitor() {
-                @Override
-                public void visitElement(@NotNull PsiElement element) {
-                    if (element instanceof MethodReference) {
-                        String phpClassFqn = null;
-
-                        String name = ((MethodReference) element).getName();
-                        if ("setDefault".equals(name) && ((MethodReference) element).getType().getTypes().stream().anyMatch(s -> s.toLowerCase().contains("optionsresolver"))) {
-                            // $resolver->setDefault('data_class', XXX);
-
-                            ParameterList parameterList = ((MethodReference) element).getParameterList();
-                            if (parameterList != null) {
-                                PsiElement parameter = parameterList.getParameter(0);
-                                if (parameter instanceof StringLiteralExpression) {
-                                    String contents = ((StringLiteralExpression) parameter).getContents();
-                                    if ("data_class".equals(contents)) {
-                                        PsiElement parameter1 = parameterList.getParameter(1);
-                                        if (parameter1 != null) {
-                                            phpClassFqn = getString(parameter1);
-                                        }
-                                    }
+            for (PhpNamedElement topLevelElement : ((PhpFile) psiFile).getTopLevelDefs().values()) {
+                if (topLevelElement instanceof PhpClass clazz) {
+                    for (Method method : clazz.getOwnMethods()) {
+                        PhpControlFlowUtil.processFlow(method.getControlFlow(), new PhpInstructionProcessor() {
+                            @Override
+                            public boolean processPhpCallInstruction(PhpCallInstruction instruction) {
+                                if (instruction.getFunctionReference() instanceof MethodReference methodReference) {
+                                    visitElement(methodReference, map);
                                 }
+                                return super.processPhpCallInstruction(instruction);
                             }
-                        } else if ("setDefaults".equals(name) && ((MethodReference) element).getType().getTypes().stream().anyMatch(s -> s.toLowerCase().contains("optionsresolver"))) {
-                            // $resolver->setDefaults(['data_class' => XXX]);
-
-                            ParameterList parameterList = ((MethodReference) element).getParameterList();
-                            if (parameterList != null) {
-                                PsiElement parameter = parameterList.getParameter(0);
-                                if (parameter instanceof ArrayCreationExpression) {
-                                    PhpPsiElement dataClassPsiElement = PhpElementsUtil.getArrayValue((ArrayCreationExpression) parameter, "data_class");
-                                    if (dataClassPsiElement != null) {
-                                        phpClassFqn = getString(dataClassPsiElement);
-                                    }
-                                }
-                            }
-                        }
-
-                        if (phpClassFqn != null) {
-                            Method methodScope = PsiTreeUtil.getParentOfType(element, Method.class);
-                            if (methodScope != null) {
-                                PhpClass parentOfType = methodScope.getContainingClass();
-                                if (parentOfType != null) {
-                                    map.putIfAbsent(phpClassFqn, new HashSet<>());
-                                    map.get(phpClassFqn).add(parentOfType.getFQN());
-                                }
-                            }
-                        }
+                        });
                     }
-
-                    super.visitElement(element);
                 }
-
-                @Nullable
-                private String getString(@NotNull PsiElement parameter) {
-                    if (parameter instanceof ClassConstantReference) {
-                        String classConstantPhpFqn = PhpElementsUtil.getClassConstantPhpFqn((ClassConstantReference) parameter);
-                        if (StringUtils.isNotBlank(classConstantPhpFqn)) {
-                            return "\\" + StringUtils.stripStart(classConstantPhpFqn, "\\");
-                        }
-                    } else if (parameter instanceof StringLiteralExpression) {
-                        String contents1 = ((StringLiteralExpression) parameter).getContents();
-                        if (StringUtils.isNotBlank(contents1)) {
-                            return "\\" + StringUtils.stripStart(contents1, "\\");
-                        }
-                    }
-
-                    return null;
-                }
-            });
+            }
 
             return map;
         };
 
+    }
+    public void visitElement(@NotNull MethodReference element, @NotNull Map<String, Set<String>> map) {
+        String phpClassFqn = null;
+
+        String name = element.getName();
+        if ("setDefault".equals(name) && element.getType().getTypes().stream().anyMatch(s -> s.toLowerCase().contains("optionsresolver"))) {
+            // $resolver->setDefault('data_class', XXX);
+
+            ParameterList parameterList = element.getParameterList();
+            if (parameterList != null) {
+                PsiElement parameter = parameterList.getParameter(0);
+                if (parameter instanceof StringLiteralExpression) {
+                    String contents = ((StringLiteralExpression) parameter).getContents();
+                    if ("data_class".equals(contents)) {
+                        PsiElement parameter1 = parameterList.getParameter(1);
+                        if (parameter1 != null) {
+                            phpClassFqn = getString(parameter1);
+                        }
+                    }
+                }
+            }
+        } else if ("setDefaults".equals(name) && element.getType().getTypes().stream().anyMatch(s -> s.toLowerCase().contains("optionsresolver"))) {
+            // $resolver->setDefaults(['data_class' => XXX]);
+
+            ParameterList parameterList = ((MethodReference) element).getParameterList();
+            if (parameterList != null) {
+                PsiElement parameter = parameterList.getParameter(0);
+                if (parameter instanceof ArrayCreationExpression) {
+                    PhpPsiElement dataClassPsiElement = PhpElementsUtil.getArrayValue((ArrayCreationExpression) parameter, "data_class");
+                    if (dataClassPsiElement != null) {
+                        phpClassFqn = getString(dataClassPsiElement);
+                    }
+                }
+            }
+        }
+
+        if (phpClassFqn != null) {
+            Method methodScope = PsiTreeUtil.getParentOfType(element, Method.class);
+            if (methodScope != null) {
+                PhpClass parentOfType = methodScope.getContainingClass();
+                if (parentOfType != null) {
+                    map.putIfAbsent(phpClassFqn, new HashSet<>());
+                    map.get(phpClassFqn).add(parentOfType.getFQN());
+                }
+            }
+        }
+    }
+
+    @Nullable
+    private static String getString(@NotNull PsiElement parameter) {
+        if (parameter instanceof ClassConstantReference) {
+            String classConstantPhpFqn = PhpElementsUtil.getClassConstantPhpFqn((ClassConstantReference) parameter);
+            if (StringUtils.isNotBlank(classConstantPhpFqn)) {
+                return "\\" + StringUtils.stripStart(classConstantPhpFqn, "\\");
+            }
+        } else if (parameter instanceof StringLiteralExpression) {
+            String contents1 = ((StringLiteralExpression) parameter).getContents();
+            if (StringUtils.isNotBlank(contents1)) {
+                return "\\" + StringUtils.stripStart(contents1, "\\");
+            }
+        }
+
+        return null;
     }
 
     @NotNull
