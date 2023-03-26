8fb2e7402b36aabbd468671345756f3ecad41654
Daniel Espendiller <daniel@espendiller.net>
2023-03-26 11:18:05 +0200

    replace recursive visiting for service tag "extends" inspection

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/codeInspection/service/TaggedExtendsInterfaceClassInspection.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/codeInspection/service/TaggedExtendsInterfaceClassInspection.java
@@ -3,12 +3,12 @@ package fr.adrienbrault.idea.symfony2plugin.codeInspection.service;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.lang.Language;
+import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiRecursiveElementWalkingVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
@@ -23,9 +23,9 @@ import fr.adrienbrault.idea.symfony2plugin.util.yaml.YamlHelper;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.yaml.YAMLLanguage;
 import org.jetbrains.yaml.YAMLTokenTypes;
 import org.jetbrains.yaml.psi.YAMLCompoundValue;
-import org.jetbrains.yaml.psi.YAMLFile;
 import org.jetbrains.yaml.psi.YAMLKeyValue;
 import org.jetbrains.yaml.psi.YAMLScalar;
 
@@ -39,107 +39,93 @@ public class TaggedExtendsInterfaceClassInspection extends LocalInspectionTool {
     @NotNull
     @Override
     public PsiElementVisitor buildVisitor(final @NotNull ProblemsHolder holder, boolean isOnTheFly) {
-        if(!Symfony2ProjectComponent.isEnabled(holder.getProject())) {
+        if (!Symfony2ProjectComponent.isEnabled(holder.getProject())) {
             return super.buildVisitor(holder, isOnTheFly);
         }
 
         return new PsiElementVisitor() {
+            private NotNullLazyValue<ContainerCollectionResolver.LazyServiceCollector> serviceCollector;
+
             @Override
-            public void visitFile(@NotNull PsiFile psiFile) {
-                if(psiFile instanceof YAMLFile) {
-                    psiFile.acceptChildren(new YmlClassElementWalkingVisitor(holder, new ContainerCollectionResolver.LazyServiceCollector(holder.getProject())));
-                } else if(psiFile instanceof XmlFile) {
-                    psiFile.acceptChildren(new XmlClassElementWalkingVisitor(holder, new ContainerCollectionResolver.LazyServiceCollector(holder.getProject())));
+            public void visitElement(@NotNull PsiElement element) {
+                Language language = element.getLanguage();
+
+                if (language == YAMLLanguage.INSTANCE) {
+                    visitYamlElement(element, holder, this.createLazyServiceCollector());
+                } else if (language == XMLLanguage.INSTANCE) {
+                    visitXmlElement(element, holder, this.createLazyServiceCollector());
                 }
+
+                super.visitElement(element);
+            }
+
+            private NotNullLazyValue<ContainerCollectionResolver.LazyServiceCollector> createLazyServiceCollector() {
+                if (this.serviceCollector == null) {
+                    this.serviceCollector = NotNullLazyValue.lazy(() -> new ContainerCollectionResolver.LazyServiceCollector(holder.getProject()));
+                }
+
+                return this.serviceCollector;
             }
         };
     }
 
-    private class XmlClassElementWalkingVisitor extends PsiRecursiveElementWalkingVisitor {
-        private final ProblemsHolder holder;
-        private final ContainerCollectionResolver.LazyServiceCollector lazyServiceCollector;
-
-        public XmlClassElementWalkingVisitor(ProblemsHolder holder, ContainerCollectionResolver.LazyServiceCollector lazyServiceCollector) {
-            this.holder = holder;
-            this.lazyServiceCollector = lazyServiceCollector;
-        }
-
-        @Override
-        public void visitElement(@NotNull PsiElement element) {
-            String className = getClassNameFromServiceDefinition(element);
-            if (className != null) {
-                XmlTag parentOfType = PsiTreeUtil.getParentOfType(element, XmlTag.class);
-                if(parentOfType != null) {
-                    // attach problems to string value only
-                    PsiElement[] psiElements = element.getChildren();
-                    if (psiElements.length > 2) {
-                        registerTaggedProblems(psiElements[1], FormUtil.getTags(parentOfType), className, holder, this.lazyServiceCollector);
-                    }
+    private void visitXmlElement(@NotNull PsiElement element, @NotNull ProblemsHolder holder, @NotNull NotNullLazyValue<ContainerCollectionResolver.LazyServiceCollector> lazyServiceCollector) {
+        String className = getClassNameFromServiceDefinition(element);
+        if (className != null) {
+            XmlTag parentOfType = PsiTreeUtil.getParentOfType(element, XmlTag.class);
+            if (parentOfType != null) {
+                // attach problems to string value only
+                PsiElement[] psiElements = element.getChildren();
+                if (psiElements.length > 2) {
+                    registerTaggedProblems(psiElements[1], FormUtil.getTags(parentOfType), className, holder, lazyServiceCollector);
                 }
             }
-
-            super.visitElement(element);
         }
     }
 
-    private class YmlClassElementWalkingVisitor extends PsiRecursiveElementWalkingVisitor {
-        private final ProblemsHolder holder;
-        private final ContainerCollectionResolver.LazyServiceCollector lazyServiceCollector;
+    private void visitYamlElement(@NotNull PsiElement psiElement, @NotNull ProblemsHolder holder, @NotNull NotNullLazyValue<ContainerCollectionResolver.LazyServiceCollector> lazyServiceCollector) {
+        if (YamlElementPatternHelper.getSingleLineScalarKey("class").accepts(psiElement)) {
 
-        public YmlClassElementWalkingVisitor(ProblemsHolder holder, ContainerCollectionResolver.LazyServiceCollector lazyServiceCollector) {
-            this.holder = holder;
-            this.lazyServiceCollector = lazyServiceCollector;
-        }
+            // class: '\Foo'
+            String text = PsiElementUtils.trimQuote(psiElement.getText());
+            if (StringUtils.isBlank(text)) {
+                return;
+            }
 
-        @Override
-        public void visitElement(@NotNull PsiElement psiElement) {
-            if(YamlElementPatternHelper.getSingleLineScalarKey("class").accepts(psiElement)) {
+            PsiElement yamlScalar = psiElement.getParent();
+            if (!(yamlScalar instanceof YAMLScalar)) {
+                return;
+            }
 
-                // class: '\Foo'
-                String text = PsiElementUtils.trimQuote(psiElement.getText());
-                if(StringUtils.isBlank(text)) {
-                    super.visitElement(psiElement);
-                    return;
-                }
-
-                PsiElement yamlScalar = psiElement.getParent();
-                if(!(yamlScalar instanceof YAMLScalar)) {
-                    super.visitElement(psiElement);
-                    return;
-                }
-
-                PsiElement classKey = yamlScalar.getParent();
-                if(classKey instanceof YAMLKeyValue) {
-                    PsiElement yamlCompoundValue = classKey.getParent();
-                    if(yamlCompoundValue instanceof YAMLCompoundValue) {
-                        PsiElement serviceKeyValue = yamlCompoundValue.getParent();
-                        if(serviceKeyValue instanceof YAMLKeyValue) {
-                            Set<String> tags = YamlHelper.collectServiceTags((YAMLKeyValue) serviceKeyValue);
-                            if(tags.size() > 0) {
-                                registerTaggedProblems(psiElement, tags, text, holder, this.lazyServiceCollector);
-                            }
-                        }
-                    }
-                }
-            } else if (psiElement.getNode().getElementType() == YAMLTokenTypes.SCALAR_KEY && YamlElementPatternHelper.getServiceIdKeyValuePattern().accepts(psiElement.getParent())) {
-                // Foobar\Foo: ~
-                String text = PsiElementUtils.getText(psiElement);
-                if (StringUtils.isNotBlank(text) && YamlHelper.isClassServiceId(text) && text.contains("\\")) {
-                    PsiElement yamlKeyValue = psiElement.getParent();
-                    if (yamlKeyValue instanceof YAMLKeyValue && YamlHelper.getYamlKeyValue((YAMLKeyValue) yamlKeyValue, "resource") == null && YamlHelper.getYamlKeyValue((YAMLKeyValue) yamlKeyValue, "exclude") == null) {
-                        Set<String> tags = YamlHelper.collectServiceTags((YAMLKeyValue) yamlKeyValue);
-                        if(tags.size() > 0) {
-                            registerTaggedProblems(psiElement, tags, text, holder, this.lazyServiceCollector);
+            PsiElement classKey = yamlScalar.getParent();
+            if (classKey instanceof YAMLKeyValue) {
+                PsiElement yamlCompoundValue = classKey.getParent();
+                if (yamlCompoundValue instanceof YAMLCompoundValue) {
+                    PsiElement serviceKeyValue = yamlCompoundValue.getParent();
+                    if (serviceKeyValue instanceof YAMLKeyValue) {
+                        Set<String> tags = YamlHelper.collectServiceTags((YAMLKeyValue) serviceKeyValue);
+                        if (tags.size() > 0) {
+                            registerTaggedProblems(psiElement, tags, text, holder, lazyServiceCollector);
                         }
                     }
                 }
             }
-
-            super.visitElement(psiElement);
+        } else if (psiElement.getNode().getElementType() == YAMLTokenTypes.SCALAR_KEY && YamlElementPatternHelper.getServiceIdKeyValuePattern().accepts(psiElement.getParent())) {
+            // Foobar\Foo: ~
+            String text = PsiElementUtils.getText(psiElement);
+            if (StringUtils.isNotBlank(text) && YamlHelper.isClassServiceId(text) && text.contains("\\")) {
+                PsiElement yamlKeyValue = psiElement.getParent();
+                if (yamlKeyValue instanceof YAMLKeyValue && YamlHelper.getYamlKeyValue((YAMLKeyValue) yamlKeyValue, "resource") == null && YamlHelper.getYamlKeyValue((YAMLKeyValue) yamlKeyValue, "exclude") == null) {
+                    Set<String> tags = YamlHelper.collectServiceTags((YAMLKeyValue) yamlKeyValue);
+                    if (tags.size() > 0) {
+                        registerTaggedProblems(psiElement, tags, text, holder, lazyServiceCollector);
+                    }
+                }
+            }
         }
     }
 
-    private void registerTaggedProblems(@NotNull PsiElement source, @NotNull Set<String> tags, @NotNull String serviceClass, @NotNull ProblemsHolder holder, @NotNull ContainerCollectionResolver.LazyServiceCollector lazyServiceCollector) {
+    private void registerTaggedProblems(@NotNull PsiElement source, @NotNull Set<String> tags, @NotNull String serviceClass, @NotNull ProblemsHolder holder, @NotNull NotNullLazyValue<ContainerCollectionResolver.LazyServiceCollector> lazyServiceCollector) {
         if (tags.size() == 0) {
             return;
         }
@@ -151,9 +137,9 @@ public class TaggedExtendsInterfaceClassInspection extends LocalInspectionTool {
 
             for (String expectedClass : ServiceUtil.TAG_INTERFACES.getOrDefault(tag, new String[] {})) {
                 // load PhpClass only if we need it, on error exit
-                if(phpClass == null) {
-                    phpClass = ServiceUtil.getResolvedClassDefinition(holder.getProject(), serviceClass, lazyServiceCollector);
-                    if(phpClass == null) {
+                if (phpClass == null) {
+                    phpClass = ServiceUtil.getResolvedClassDefinition(holder.getProject(), serviceClass, lazyServiceCollector.get());
+                    if (phpClass == null) {
                         return;
                     }
                 }
@@ -164,7 +150,7 @@ public class TaggedExtendsInterfaceClassInspection extends LocalInspectionTool {
                 }
 
                 // check interfaces
-                if(!PhpElementsUtil.isInstanceOf(phpClass, expectedClass)) {
+                if (!PhpElementsUtil.isInstanceOf(phpClass, expectedClass)) {
                     missingTagInstance = expectedClass;
                     continue;
                 }
