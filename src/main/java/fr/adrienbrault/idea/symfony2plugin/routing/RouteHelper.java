aaba7760db229ec3abd7570b6812e14890cc6884
Benoit Duffez <b.duffez@kerlink.com>
2023-12-14 05:17:37 -0800

    optimized cache/search

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/routing/RouteHelper.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/routing/RouteHelper.java
@@ -492,7 +492,6 @@ public class RouteHelper {
     @NotNull
     public static Map<String, Route> getRoutesInsideUrlGeneratorFile(@NotNull PsiFile psiFile) {
         Map<String, Route> routes = new HashMap<>();
-        HashSet<String> canonicalRoutes = new HashSet<>();
 
         // Symfony >= 4
         // extract the routes on a return statement
@@ -521,8 +520,7 @@ public class RouteHelper {
                             PhpPsiElement value = e.getValue();
                             if (value != null) {
                                 String canonical = value.getText().replace("'", "");
-                                if (!canonicalRoutes.contains(canonical)) {
-                                    canonicalRoutes.add(canonical);
+                                if (!routes.containsKey(canonical)) {
                                     routes.put(canonical, route);
                                 }
                             }
