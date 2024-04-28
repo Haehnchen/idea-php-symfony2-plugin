6c7f1565b582b5ba92a8f355fb40ae1bfde7d9c5
Daniel Espendiller <daniel@espendiller.net>
2024-04-28 12:46:32 +0200

    provide inspection language attributes for deprecated class and controller method inspections

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/routing/inspection/ControllerMethodInspection.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/routing/inspection/ControllerMethodInspection.java
@@ -2,8 +2,6 @@ package fr.adrienbrault.idea.symfony2plugin.routing.inspection;
 
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.lang.Language;
-import com.intellij.lang.xml.XMLLanguage;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -18,90 +16,90 @@ import fr.adrienbrault.idea.symfony2plugin.util.yaml.YamlHelper;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.yaml.YAMLLanguage;
 import org.jetbrains.yaml.psi.YAMLKeyValue;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
-public class ControllerMethodInspection extends LocalInspectionTool {
+public class ControllerMethodInspection {
+    public static class ControllerMethodInspectionYaml extends LocalInspectionTool {
+        public @NotNull PsiElementVisitor buildVisitor(final @NotNull ProblemsHolder holder, boolean isOnTheFly) {
+            if (!Symfony2ProjectComponent.isEnabled(holder.getProject())) {
+                return super.buildVisitor(holder, isOnTheFly);
+            }
 
-    @NotNull
-    @Override
-    public PsiElementVisitor buildVisitor(final @NotNull ProblemsHolder holder, boolean isOnTheFly) {
-        if(!Symfony2ProjectComponent.isEnabled(holder.getProject())) {
-            return super.buildVisitor(holder, isOnTheFly);
-        }
+            return new PsiElementVisitor() {
+                @Override
+                public void visitElement(@NotNull PsiElement element) {
+                    if (YamlElementPatternHelper.getSingleLineScalarKey("_controller", "controller").accepts(element)) {
+                        String text = PsiElementUtils.trimQuote(element.getText());
+                        if (StringUtils.isNotBlank(text)) {
+                            InspectionUtil.inspectController(element, text, holder, new YamlLazyRouteName(element));
+                        }
+                    }
 
-        return new PsiElementVisitor() {
-            @Override
-            public void visitElement(@NotNull PsiElement element) {
-                Language language = element.getLanguage();
-
-                if (language == YAMLLanguage.INSTANCE) {
-                    visitYamlElement(element, holder);
-                } else if(language == XMLLanguage.INSTANCE) {
-                    visitXmlElement(element, holder);
+                    super.visitElement(element);
                 }
-
-                super.visitElement(element);
-            }
-        };
-    }
-
-    private void visitYamlElement(@NotNull PsiElement element, @NotNull ProblemsHolder holder) {
-        if (YamlElementPatternHelper.getSingleLineScalarKey("_controller", "controller").accepts(element)) {
-            String text = PsiElementUtils.trimQuote(element.getText());
-            if (StringUtils.isNotBlank(text)) {
-                InspectionUtil.inspectController(element, text, holder, new YamlLazyRouteName(element));
-            }
+            };
         }
     }
 
-    public void visitXmlElement(@NotNull PsiElement element, @NotNull ProblemsHolder holder) {
-        if(XmlHelper.getRouteControllerPattern().accepts(element)) {
-            String text = PsiElementUtils.trimQuote(element.getText());
-            if(StringUtils.isNotBlank(text)) {
-                InspectionUtil.inspectController(element, text, holder, new XmlLazyRouteName(element));
+    public static class ControllerMethodInspectionXml extends LocalInspectionTool {
+        public @NotNull PsiElementVisitor buildVisitor(final @NotNull ProblemsHolder holder, boolean isOnTheFly) {
+            if (!Symfony2ProjectComponent.isEnabled(holder.getProject())) {
+                return super.buildVisitor(holder, isOnTheFly);
             }
+
+            return new PsiElementVisitor() {
+                @Override
+                public void visitElement(@NotNull PsiElement element) {
+                    if(XmlHelper.getRouteControllerPattern().accepts(element)) {
+                        String text = PsiElementUtils.trimQuote(element.getText());
+                        if(StringUtils.isNotBlank(text)) {
+                            InspectionUtil.inspectController(element, text, holder, new XmlLazyRouteName(element));
+                        }
+                    }
+
+                    super.visitElement(element);
+                }
+            };
         }
     }
 
     private record YamlLazyRouteName(@NotNull PsiElement psiElement) implements InspectionUtil.LazyControllerNameResolve {
         @Nullable
-            @Override
-            public String getRouteName() {
-                YAMLKeyValue defaultKeyValue = PsiTreeUtil.getParentOfType(this.psiElement.getParent(), YAMLKeyValue.class);
-                if (defaultKeyValue == null) {
-                    return null;
-                }
-
-                YAMLKeyValue def = PsiTreeUtil.getParentOfType(defaultKeyValue, YAMLKeyValue.class);
-                if (def == null) {
-                    return null;
-                }
-
-                return YamlHelper.getYamlKeyName(def);
-            }
-        }
-
-    private record XmlLazyRouteName(@NotNull PsiElement psiElement) implements InspectionUtil.LazyControllerNameResolve {
-
-        @Nullable
-            @Override
-            public String getRouteName() {
-                XmlTag defaultTag = PsiTreeUtil.getParentOfType(this.psiElement, XmlTag.class);
-                if (defaultTag != null) {
-                    XmlTag routeTag = PsiTreeUtil.getParentOfType(defaultTag, XmlTag.class);
-                    if (routeTag != null) {
-                        XmlAttribute id = routeTag.getAttribute("id");
-                        if (id != null) {
-                            return id.getValue();
-                        }
-                    }
-                }
-
+        @Override
+        public String getRouteName() {
+            YAMLKeyValue defaultKeyValue = PsiTreeUtil.getParentOfType(this.psiElement.getParent(), YAMLKeyValue.class);
+            if (defaultKeyValue == null) {
                 return null;
             }
+
+            YAMLKeyValue def = PsiTreeUtil.getParentOfType(defaultKeyValue, YAMLKeyValue.class);
+            if (def == null) {
+                return null;
+            }
+
+            return YamlHelper.getYamlKeyName(def);
         }
+    }
+
+    private record XmlLazyRouteName(@NotNull PsiElement psiElement) implements InspectionUtil.LazyControllerNameResolve {
+        @Nullable
+        @Override
+        public String getRouteName() {
+            XmlTag defaultTag = PsiTreeUtil.getParentOfType(this.psiElement, XmlTag.class);
+            if (defaultTag != null) {
+                XmlTag routeTag = PsiTreeUtil.getParentOfType(defaultTag, XmlTag.class);
+                if (routeTag != null) {
+                    XmlAttribute id = routeTag.getAttribute("id");
+                    if (id != null) {
+                        return id.getValue();
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
 }
