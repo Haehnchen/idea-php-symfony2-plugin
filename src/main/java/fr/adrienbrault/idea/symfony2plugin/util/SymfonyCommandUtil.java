f9a9dcbd07719b0099075c61ba0294ac823f5d44
Daniel Espendiller <daniel@espendiller.net>
2025-12-11 17:35:11 +0100

    Refactor Symfony commands `options` and `arguments` extraction, supporting both traditional and modern approaches

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/util/SymfonyCommandUtil.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/util/SymfonyCommandUtil.java
@@ -2,19 +2,21 @@ package fr.adrienbrault.idea.symfony2plugin.util;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.CachedValue;
 import com.intellij.psi.util.CachedValueProvider;
 import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.psi.util.PsiModificationTracker;
-import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.lang.psi.stubs.indexes.expectedArguments.PhpExpectedFunctionScalarArgument;
 import fr.adrienbrault.idea.symfony2plugin.dic.command.SymfonyCommandTestRunLineMarkerProvider;
 import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.PhpAttributeIndexUtil;
 import fr.adrienbrault.idea.symfony2plugin.util.dict.SymfonyCommand;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -62,5 +64,406 @@ public class SymfonyCommandUtil {
         return cachedValue.entrySet().stream()
             .map(entry -> new SymfonyCommand(entry.getKey(), entry.getValue()))
             .collect(Collectors.toList());
+    }
+
+    /**
+     * Collects all available option names and shortcuts from a command class.
+     * Supports both traditional configure() methods with addOption() calls
+     * and modern #[Option] attributes on __invoke() method parameters.
+     */
+    @NotNull
+    public static Map<String, CommandOption> getCommandOptions(@NotNull PhpClass phpClass) {
+        Map<String, CommandOption> options = new HashMap<>();
+
+        // Collect options from traditional configure() method
+        Method configureMethod = phpClass.findOwnMethodByName("configure");
+        if (configureMethod != null) {
+            // Case 1: addOption() method calls
+            Collection<MethodReference> addOptionCalls = PhpElementsUtil.collectMethodReferencesInsideControlFlow(configureMethod, "addOption");
+            for (MethodReference methodRef : addOptionCalls) {
+                CommandOption option = parseAddOptionCall(methodRef);
+                if (option != null) {
+                    options.put(option.name(), option);
+                }
+            }
+
+            // Case 2: setDefinition() with new InputOption() instances
+            Collection<MethodReference> setDefinitionCalls = PhpElementsUtil.collectMethodReferencesInsideControlFlow(configureMethod, "setDefinition");
+            for (MethodReference methodRef : setDefinitionCalls) {
+                Map<String, CommandOption> definitionOptions = parseSetDefinitionCall(methodRef);
+                options.putAll(definitionOptions);
+            }
+        }
+
+        // Collect options from modern __invoke() method with #[Option] attributes
+        Method invokeMethod = phpClass.findOwnMethodByName("__invoke");
+        if (invokeMethod != null) {
+            for (Parameter parameter : invokeMethod.getParameters()) {
+                CommandOption option = parseOptionAttribute(parameter);
+                if (option != null) {
+                    options.put(option.name(), option);
+                }
+            }
+        }
+
+        return options;
+    }
+
+    /**
+     * Parses an addOption() method call and extracts the option name, shortcut, and metadata.
+     */
+    @Nullable
+    private static CommandOption parseAddOptionCall(@NotNull MethodReference methodRef) {
+        // addOption(name, shortcut, mode, description, default)
+        PsiElement[] parameters = methodRef.getParameters();
+
+        // Get name (parameter 0)
+        if (parameters.length == 0) {
+            return null;
+        }
+
+        PsiElement nameParam = parameters[0];
+        String name = PhpElementsUtil.getStringValue(nameParam);
+        if (StringUtils.isBlank(name)) {
+            return null;
+        }
+
+        // Get shortcut (parameter 1)
+        String shortcut = null;
+        if (parameters.length > 1) {
+            shortcut = PhpElementsUtil.getStringValue(parameters[1]);
+        }
+
+        // Get description (parameter 3)
+        String description = null;
+        if (parameters.length > 3) {
+            description = PhpElementsUtil.getStringValue(parameters[3]);
+        }
+
+        // Get default value (parameter 4)
+        String defaultValue = null;
+        if (parameters.length > 4) {
+            defaultValue = parameters[4].getText();
+        }
+
+        return new CommandOption(nameParam, name, shortcut, description, defaultValue);
+    }
+
+    /**
+     * Parses a setDefinition() method call and extracts options from new InputOption() instances.
+     * Handles both single instances and arrays of instances.
+     */
+    @NotNull
+    private static Map<String, CommandOption> parseSetDefinitionCall(@NotNull MethodReference methodRef) {
+        Map<String, CommandOption> options = new HashMap<>();
+
+        // Collect all new InputOption() expressions within the setDefinition call
+        Collection<NewExpression> newExpressions = com.intellij.psi.util.PsiTreeUtil.collectElementsOfType(methodRef, NewExpression.class);
+        for (NewExpression newExpression : newExpressions) {
+            // Check if this is a new InputOption() instance
+            if (PhpElementsUtil.getNewExpressionPhpClassWithInstance(newExpression, "Symfony\\Component\\Console\\Input\\InputOption") != null) {
+                CommandOption option = parseInputOptionConstructor(newExpression);
+                if (option != null) {
+                    options.put(option.name(), option);
+                }
+            }
+        }
+
+        return options;
+    }
+
+    /**
+     * Parses a new InputOption() constructor and extracts the option name, shortcut, and metadata.
+     *
+     * Constructor signature: new InputOption(name, shortcut, mode, description, default)
+     */
+    @Nullable
+    private static CommandOption parseInputOptionConstructor(@NotNull NewExpression newExpression) {
+        PsiElement[] parameters = newExpression.getParameters();
+
+        // Get name (parameter 0)
+        if (parameters.length == 0) {
+            return null;
+        }
+
+        String name = PhpElementsUtil.getStringValue(parameters[0]);
+        if (StringUtils.isBlank(name)) {
+            return null;
+        }
+
+        // Get shortcut (parameter 1)
+        String shortcut = null;
+        if (parameters.length > 1) {
+            shortcut = PhpElementsUtil.getStringValue(parameters[1]);
+        }
+
+        // Get description (parameter 3)
+        String description = null;
+        if (parameters.length > 3) {
+            description = PhpElementsUtil.getStringValue(parameters[3]);
+        }
+
+        // Get default value (parameter 4)
+        String defaultValue = null;
+        if (parameters.length > 4) {
+            defaultValue = parameters[4].getText();
+        }
+
+        return new CommandOption(parameters[0], name, shortcut, description, defaultValue);
+    }
+
+    /**
+     * Parses a #[Option] attribute from a parameter and extracts the option name, shortcut, and metadata.
+     */
+    @Nullable
+    private static CommandOption parseOptionAttribute(@NotNull Parameter parameter) {
+        // Check for Option attribute (only the valid FQN)
+        Collection<PhpAttribute> attributes = parameter.getAttributes("\\Symfony\\Component\\Console\\Attribute\\Option");
+
+        for (PhpAttribute attribute : attributes) {
+            // Extract option name, shortcut, and description from attribute arguments
+            String name = null;
+            String shortcut = null;
+            String description = null;
+
+            for (PhpAttribute.PhpAttributeArgument argument : attribute.getArguments()) {
+                String argName = argument.getName();
+
+                if (argument.getArgument() instanceof PhpExpectedFunctionScalarArgument scalarArgument) {
+                    String value = PsiElementUtils.trimQuote(scalarArgument.getNormalizedValue());
+
+                    if ("name".equals(argName)) {
+                        name = value;
+                    } else if ("shortcut".equals(argName)) {
+                        shortcut = value;
+                    } else if ("description".equals(argName)) {
+                        description = value;
+                    }
+                }
+            }
+
+            // If name is not explicitly set, use the parameter name
+            if (name == null) {
+                name = parameter.getName();
+            }
+
+            if (StringUtils.isBlank(name)) {
+                continue;
+            }
+
+            // Get default value from parameter default value
+            String defaultValue = null;
+            PsiElement defaultValueElement = parameter.getDefaultValue();
+            if (defaultValueElement != null) {
+                defaultValue = defaultValueElement.getText();
+            }
+
+            return new CommandOption(parameter, name, shortcut, description, defaultValue);
+        }
+
+        return null;
+    }
+
+    /**
+     * Collects all available arguments from a command class.
+     * Supports both traditional configure() methods with addArgument() calls,
+     * setDefinition() with new InputArgument() instances,
+     * and modern #[Argument] attributes on __invoke() method parameters.
+     */
+    @NotNull
+    public static Map<String, CommandArgument> getCommandArguments(@NotNull PhpClass phpClass) {
+        Map<String, CommandArgument> arguments = new HashMap<>();
+
+        // Collect arguments from traditional configure() method
+        Method configureMethod = phpClass.findOwnMethodByName("configure");
+        if (configureMethod != null) {
+            // Case 1: addArgument() method calls
+            Collection<MethodReference> addArgumentCalls = PhpElementsUtil.collectMethodReferencesInsideControlFlow(configureMethod, "addArgument");
+            for (MethodReference methodRef : addArgumentCalls) {
+                CommandArgument argument = parseAddArgumentCall(methodRef);
+                if (argument != null) {
+                    arguments.put(argument.name(), argument);
+                }
+            }
+
+            // Case 2: setDefinition() with new InputArgument() instances
+            Collection<MethodReference> setDefinitionCalls = PhpElementsUtil.collectMethodReferencesInsideControlFlow(configureMethod, "setDefinition");
+            for (MethodReference methodRef : setDefinitionCalls) {
+                Map<String, CommandArgument> definitionArguments = parseSetDefinitionCallForArguments(methodRef);
+                arguments.putAll(definitionArguments);
+            }
+        }
+
+        // Collect arguments from modern __invoke() method with #[Argument] attributes
+        Method invokeMethod = phpClass.findOwnMethodByName("__invoke");
+        if (invokeMethod != null) {
+            for (Parameter parameter : invokeMethod.getParameters()) {
+                CommandArgument argument = parseArgumentAttribute(parameter);
+                if (argument != null) {
+                    arguments.put(argument.name(), argument);
+                }
+            }
+        }
+
+        return arguments;
+    }
+
+    /**
+     * Parses an addArgument() method call and extracts the argument metadata.
+     */
+    @Nullable
+    private static CommandArgument parseAddArgumentCall(@NotNull MethodReference methodRef) {
+        // addArgument(name, mode, description, default)
+        PsiElement[] parameters = methodRef.getParameters();
+
+        // Get name (parameter 0)
+        if (parameters.length == 0) {
+            return null;
+        }
+
+        PsiElement nameParam = parameters[0];
+        String name = PhpElementsUtil.getStringValue(nameParam);
+        if (StringUtils.isBlank(name)) {
+            return null;
+        }
+
+        // Get description (parameter 2)
+        String description = null;
+        if (parameters.length > 2) {
+            description = PhpElementsUtil.getStringValue(parameters[2]);
+        }
+
+        // Get default value (parameter 3) - keep as text representation
+        String defaultValue = null;
+        if (parameters.length > 3) {
+            defaultValue = parameters[3].getText();
+        }
+
+        return new CommandArgument(nameParam, name, description, defaultValue);
+    }
+
+    /**
+     * Parses a setDefinition() method call and extracts arguments from new InputArgument() instances.
+     *
+     * @param methodRef The setDefinition method reference
+     * @return Map of argument names to CommandArgument objects
+     */
+    @NotNull
+    private static Map<String, CommandArgument> parseSetDefinitionCallForArguments(@NotNull MethodReference methodRef) {
+        Map<String, CommandArgument> arguments = new HashMap<>();
+
+        // Collect all new InputArgument() expressions within the setDefinition call
+        Collection<NewExpression> newExpressions = com.intellij.psi.util.PsiTreeUtil.collectElementsOfType(methodRef, NewExpression.class);
+        for (NewExpression newExpression : newExpressions) {
+            // Check if this is a new InputArgument() instance
+            if (PhpElementsUtil.getNewExpressionPhpClassWithInstance(newExpression, "Symfony\\Component\\Console\\Input\\InputArgument") != null) {
+                CommandArgument argument = parseInputArgumentConstructor(newExpression);
+                if (argument != null) {
+                    arguments.put(argument.name(), argument);
+                }
+            }
+        }
+
+        return arguments;
+    }
+
+    /**
+     * Parses a new InputArgument() constructor and extracts the argument metadata.
+     *
+     * Constructor signature: new InputArgument(name, mode, description, default)
+     */
+    @Nullable
+    private static CommandArgument parseInputArgumentConstructor(@NotNull NewExpression newExpression) {
+        PsiElement[] parameters = newExpression.getParameters();
+
+        // Get name (parameter 0)
+        if (parameters.length == 0) {
+            return null;
+        }
+
+        String name = PhpElementsUtil.getStringValue(parameters[0]);
+        if (StringUtils.isBlank(name)) {
+            return null;
+        }
+
+        // Get description (parameter 2)
+        String description = null;
+        if (parameters.length > 2) {
+            description = PhpElementsUtil.getStringValue(parameters[2]);
+        }
+
+        // Get default value (parameter 3) - keep as text representation
+        String defaultValue = null;
+        if (parameters.length > 3) {
+            defaultValue = parameters[3].getText();
+        }
+
+        return new CommandArgument(parameters[0], name, description, defaultValue);
+    }
+
+    /**
+     * Parses a #[Argument] attribute from a parameter and extracts the argument metadata.
+     */
+    @Nullable
+    private static CommandArgument parseArgumentAttribute(@NotNull Parameter parameter) {
+        // Check for Argument attribute
+        Collection<PhpAttribute> attributes = parameter.getAttributes("\\Symfony\\Component\\Console\\Attribute\\Argument");
+
+        for (PhpAttribute attribute : attributes) {
+            // Extract argument name and description from attribute arguments
+            String name = null;
+            String description = null;
+
+            for (PhpAttribute.PhpAttributeArgument argument : attribute.getArguments()) {
+                String argName = argument.getName();
+
+                if (argument.getArgument() instanceof PhpExpectedFunctionScalarArgument scalarArgument) {
+                    String value = PsiElementUtils.trimQuote(scalarArgument.getNormalizedValue());
+
+                    if ("name".equals(argName)) {
+                        name = value;
+                    } else if ("description".equals(argName)) {
+                        description = value;
+                    }
+                }
+            }
+
+            // If name is not explicitly set, use the parameter name
+            if (name == null) {
+                name = parameter.getName();
+            }
+
+            if (StringUtils.isBlank(name)) {
+                continue;
+            }
+
+            // Get default value from parameter default value
+            String defaultValue = null;
+            PsiElement defaultValueElement = parameter.getDefaultValue();
+            if (defaultValueElement != null) {
+                defaultValue = defaultValueElement.getText();
+            }
+
+            return new CommandArgument(parameter, name, description, defaultValue);
+        }
+
+        return null;
+    }
+
+    public record CommandOption(
+        @NotNull PsiElement target,
+        @NotNull String name,
+        @Nullable String shortcut,
+        @Nullable String description,
+        @Nullable String defaultValue
+    ) {
+    }
+
+    public record CommandArgument(
+        @NotNull PsiElement target,
+        @NotNull String name,
+        @Nullable String description,
+        @Nullable String defaultValue
+    ) {
     }
 }
--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/util/fixtures/SymfonyCommandUtilTest.php
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/util/fixtures/SymfonyCommandUtilTest.php
@@ -52,3 +52,217 @@ namespace Foo
         protected static $defaultName = 'app:create-user-3';
     }
 }
+
+namespace Symfony\Component\Console\Attribute
+{
+    class AsCommand {}
+    class Option {}
+}
+
+namespace Symfony\Component\Console\Input\Attribute
+{
+    class Option {}
+}
+
+namespace Symfony\Component\Console\Input
+{
+    class InputInterface {}
+    class InputOption {}
+}
+
+namespace Symfony\Component\Console\Output
+{
+    class OutputInterface {}
+}
+
+namespace Symfony\Component\Console\Style
+{
+    class SymfonyStyle {}
+}
+
+namespace CommandOptions
+{
+    use Symfony\Component\Console\Attribute\AsCommand;
+    use Symfony\Component\Console\Attribute\Option;
+    use Symfony\Component\Console\Command\Command;
+    use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Input\InputOption;
+    use Symfony\Component\Console\Output\OutputInterface;
+    use Symfony\Component\Console\Style\SymfonyStyle;
+
+    // Traditional command with addOption() calls
+    #[AsCommand(name: 'app:traditional')]
+    class TraditionalCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this
+                ->addOption('name', null, InputOption::VALUE_NONE)
+                ->addOption('last_name', 'd', InputOption::VALUE_NONE);
+        }
+
+        public function execute(InputInterface $input, OutputInterface $output): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Modern invokable command with #[Option] attributes
+    #[AsCommand(name: 'app:modern')]
+    class ModernCommand
+    {
+        public function __invoke(
+            SymfonyStyle $io,
+            #[Option(name: 'idle')] ?int $timeout = null,
+            #[Option] string $type = 'USER_TYPE',
+            #[Option(shortcut: 'v')] bool $verbose = false,
+            #[Option(description: 'User groups')] array $groups = [],
+        ): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Mixed: traditional configure + invokable
+    #[AsCommand(name: 'app:mixed')]
+    class MixedCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this->addOption('config', 'c', InputOption::VALUE_REQUIRED);
+        }
+
+        public function __invoke(
+            SymfonyStyle $io,
+            #[Option] bool $force = false,
+        ): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Using setDefinition() with new InputOption()
+    #[AsCommand(name: 'app:definition')]
+    class DefinitionCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this->setDefinition([
+                new InputOption('format', 'f', InputOption::VALUE_REQUIRED),
+                new InputOption('verbose', 'v', InputOption::VALUE_NONE),
+            ]);
+        }
+
+        public function execute(InputInterface $input, OutputInterface $output): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Using setDefinition() with single InputOption
+    #[AsCommand(name: 'app:definition-single')]
+    class DefinitionSingleCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this->setDefinition(
+                new InputOption('debug', 'd', InputOption::VALUE_NONE)
+            );
+        }
+
+        public function execute(InputInterface $input, OutputInterface $output): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+}
+
+namespace Symfony\Component\Console\Input
+{
+    class InputArgument {}
+}
+
+namespace Symfony\Component\Console\Attribute
+{
+    class Argument {}
+}
+
+namespace CommandArguments
+{
+    use Symfony\Component\Console\Attribute\AsCommand;
+    use Symfony\Component\Console\Attribute\Argument;
+    use Symfony\Component\Console\Command\Command;
+    use Symfony\Component\Console\Input\InputArgument;
+    use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Output\OutputInterface;
+    use Symfony\Component\Console\Style\SymfonyStyle;
+
+    // Traditional command with addArgument() calls
+    #[AsCommand(name: 'app:traditional-args')]
+    class TraditionalArgsCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this
+                ->addArgument('username', InputArgument::REQUIRED, 'The username')
+                ->addArgument('password', InputArgument::OPTIONAL, 'The password', 'default123');
+        }
+
+        public function execute(InputInterface $input, OutputInterface $output): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Modern invokable command with #[Argument] attributes
+    #[AsCommand(name: 'app:modern-args')]
+    class ModernArgsCommand
+    {
+        public function __invoke(
+            SymfonyStyle $io,
+            #[Argument(description: 'The user name')] string $name,
+            #[Argument(name: 'user-id', description: 'The user ID')] int $userId,
+            #[Argument] ?string $email = null,
+        ): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Using setDefinition() with new InputArgument()
+    #[AsCommand(name: 'app:definition-args')]
+    class DefinitionArgsCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this->setDefinition([
+                new InputArgument('source', InputArgument::REQUIRED, 'Source file'),
+                new InputArgument('destination', InputArgument::OPTIONAL, 'Destination file', '/tmp/default'),
+            ]);
+        }
+
+        public function execute(InputInterface $input, OutputInterface $output): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+
+    // Mixed: traditional configure + invokable with arguments
+    #[AsCommand(name: 'app:mixed-args')]
+    class MixedArgsCommand extends Command
+    {
+        protected function configure(): void
+        {
+            $this->addArgument('config-file', InputArgument::REQUIRED, 'Config file path');
+        }
+
+        public function __invoke(
+            SymfonyStyle $io,
+            #[Argument(name: 'config-file', description: 'Config file path')] string $configFile,
+            #[Argument] bool $force = false,
+        ): int
+        {
+            return Command::SUCCESS;
+        }
+    }
+}
