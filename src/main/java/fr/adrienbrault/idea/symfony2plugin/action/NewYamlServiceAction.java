b4f4f5b509d76dd7bb7f19f0c96c0f965a2f13f9
Daniel Espendiller <daniel@espendiller.net>
2026-02-26 20:20:53 +0100

    fix Symfony New Service actions showing outside project view context

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/action/NewYamlServiceAction.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/action/NewYamlServiceAction.java
@@ -4,6 +4,8 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
+import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -12,6 +14,19 @@ public class NewYamlServiceAction extends AbstractProjectDumbAwareAction {
 
     public NewYamlServiceAction() {
         super("Yaml Service", "Create new Yaml File", AllIcons.Nodes.DataTables);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        this.setStatus(event, false);
+        Project project = getEventProject(event);
+        if (!Symfony2ProjectComponent.isEnabled(project)) {
+            return;
+        }
+
+        if (NewFileActionUtil.getSelectedDirectoryFromAction(event) != null) {
+            this.setStatus(event, true);
+        }
     }
 
     @Override
