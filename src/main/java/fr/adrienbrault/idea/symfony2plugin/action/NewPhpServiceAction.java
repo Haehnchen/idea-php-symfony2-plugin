3792bf29e19864c59a48007791d31aef7afdc125
Daniel Espendiller <daniel@espendiller.net>
2026-02-26 20:20:53 +0100

    fix Symfony New Service actions showing outside project view context

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/action/NewPhpServiceAction.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/action/NewPhpServiceAction.java
@@ -4,6 +4,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.PhpIcons;
+import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -12,6 +14,19 @@ public class NewPhpServiceAction extends AbstractProjectDumbAwareAction {
 
     public NewPhpServiceAction() {
         super("PHP Service", "Create new PHP File", PhpIcons.PHP_FILE);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        this.setStatus(event, false);
+        Project project = getEventProject(event);
+        if (!Symfony2ProjectComponent.isEnabled(project)) {
+            return;
+        }
+
+        if (NewFileActionUtil.getSelectedDirectoryFromAction(event) != null) {
+            this.setStatus(event, true);
+        }
     }
 
     @Override
--- src/main/java/fr/adrienbrault/idea/symfony2plugin/action/NewXmlServiceAction.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/action/NewXmlServiceAction.java
@@ -4,6 +4,8 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
+import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
@@ -12,6 +14,19 @@ public class NewXmlServiceAction  extends AbstractProjectDumbAwareAction {
 
     public NewXmlServiceAction() {
         super("Xml Service", "Create new Xml File", AllIcons.FileTypes.Xml);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        this.setStatus(event, false);
+        Project project = getEventProject(event);
+        if (!Symfony2ProjectComponent.isEnabled(project)) {
+            return;
+        }
+
+        if (NewFileActionUtil.getSelectedDirectoryFromAction(event) != null) {
+            this.setStatus(event, true);
+        }
     }
 
     @Override
