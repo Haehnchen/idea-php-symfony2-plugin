4b69a841730c405faa30723f3de465ded699a8ed
Daniel Espendiller <daniel@espendiller.net>
2026-02-28 16:29:45 +0100

    Add unit tests for Twig render method navigation, template variable completion, and array_merge handling in controller actions

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/templating/RenderParameterGotoCompletionRegistrarTest.java
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/templating/RenderParameterGotoCompletionRegistrarTest.java
@@ -1,7 +1,10 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.templating;
 
+import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
 import com.jetbrains.php.lang.PhpFileType;
+import com.jetbrains.php.lang.psi.elements.Method;
+import com.jetbrains.twig.TwigFileType;
 import fr.adrienbrault.idea.symfony2plugin.templating.RenderParameterGotoCompletionRegistrar;
 import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
 
@@ -13,6 +16,7 @@ public class RenderParameterGotoCompletionRegistrarTest extends SymfonyLightCode
     public void setUp() throws Exception {
         super.setUp();
         myFixture.copyFileToProject("RenderParameterGotoCompletionRegistrar.php");
+        myFixture.copyFileToProject("ide-twig.json");
     }
 
     public String getTestDataPath() {
@@ -60,5 +64,125 @@ public class RenderParameterGotoCompletionRegistrarTest extends SymfonyLightCode
         PsiElement psiElement = myFixture.getFile().findElementAt(myFixture.getCaretOffset());
 
         assertContainsElements(RenderParameterGotoCompletionRegistrar.getTemplatesForScope(psiElement), "foo.html.twig");
+    }
+
+    /**
+     * Test that navigation from controller name goes to the method.
+     */
+    public void testRenderControllerNavigation() {
+        myFixture.configureByText(PhpFileType.INSTANCE, "<?php\n" +
+            "namespace App\\Controller;\n" +
+            "\n" +
+            "class NavController\n" +
+            "{\n" +
+            "    public function menuAction()\n" +
+            "    {\n" +
+            "    }\n" +
+            "}\n"
+        );
+
+        assertNavigationMatch(
+            TwigFileType.INSTANCE,
+            "{{ render(controller('App\\\\Controller\\\\Nav<caret>Controller::menuAction')) }}",
+            PlatformPatterns.psiElement(Method.class).withName("menuAction")
+        );
+    }
+
+    /**
+     * Test that template variable completion works in render() parameter array.
+     */
+    public void testTemplateVariableCompletionInRender() {
+        // Create a template with variables
+        myFixture.addFileToProject(
+            "templates/form/contact.html.twig",
+            "{# @var name string #}\n" +
+            "{# @var email string #}\n" +
+            "{# @var message string #}\n" +
+            "{{ name }}\n" +
+            "{{ email }}\n" +
+            "{{ message }}"
+        );
+
+        // Completion should suggest variables from the template
+        assertCompletionContains(
+            PhpFileType.INSTANCE,
+            "<?php $this->render('form/contact.html.twig', ['<caret>']);",
+            "name",
+            "email",
+            "message"
+        );
+    }
+
+    /**
+     * Test that template variable completion works with array_merge.
+     */
+    public void testTemplateVariableCompletionWithArrayMerge() {
+        myFixture.addFileToProject(
+            "templates/email/welcome.html.twig",
+            "{# @var user_name string #}\n" +
+            "{# @var activation_link string #}\n" +
+            "{{ user_name }}\n" +
+            "{{ activation_link }}"
+        );
+
+        assertCompletionContains(
+            PhpFileType.INSTANCE,
+            "<?php $this->render('email/welcome.html.twig', array_merge(['<caret>'], $vars));",
+            "user_name",
+            "activation_link"
+        );
+    }
+
+    /**
+     * Test that navigation from variable name goes to template usage.
+     */
+    public void testTemplateVariableNavigation() {
+        myFixture.addFileToProject(
+            "templates/view/detail.html.twig",
+            "{# @var title string #}\n" +
+            "{# @var description string #}\n" +
+            "<h1>{{ title }}</h1>\n" +
+            "<p>{{ description }}</p>"
+        );
+
+        assertNavigationMatch(
+            PhpFileType.INSTANCE,
+            "<?php $this->render('view/detail.html.twig', ['tit<caret>le' => 'My Title']);",
+            PlatformPatterns.psiElement()
+        );
+    }
+
+    /**
+     * Test that completion works in @Template annotated method return array.
+     */
+    public void testTemplateVariableCompletionInAnnotationReturn() {
+        myFixture.addFileToProject(
+            "templates/article/view.html.twig",
+            "{# @var article_title string #}\n" +
+            "{# @var article_content string #}\n" +
+            "{{ article_title }}\n" +
+            "{{ article_content }}"
+        );
+
+        assertCompletionContains(
+            PhpFileType.INSTANCE,
+            "<?php\n" +
+            "namespace App\\Controller;\n" +
+            "\n" +
+            "use Sensio\\Bundle\\FrameworkExtraBundle\\Configuration\\Template;\n" +
+            "\n" +
+            "class ArticleController\n" +
+            "{\n" +
+            "    /**\n" +
+            "     * @Template(\"article/view.html.twig\")\n" +
+            "     */\n" +
+            "    public function viewAction()\n" +
+            "    {\n" +
+            "        return ['<caret>'];\n" +
+            "    }\n" +
+            "}\n",
+            "article_title",
+            "article_content"
+        );
     }
 }
