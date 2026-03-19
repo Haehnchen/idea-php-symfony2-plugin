40cc1b05e38b9b0c34237aa22d0a80b954de30e5
Daniel Espendiller <daniel@espendiller.net>
2026-03-19 18:15:57 +0100

    Support component tag and name updates in `TwigTemplateUsageReference` during file rename and move refactoring Add `TwigMoveFileHandler` to handle logical template path updates during Twig file move refactoring Support template path updates in `TwigTemplateUsageReference` during file move refactoring Handle rebinding in `TwigTemplateUsageReference` to prevent `IncorrectOperationException` during refactoring

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/templating/TemplateReference.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/templating/TemplateReference.java
@@ -1,10 +1,14 @@
 package fr.adrienbrault.idea.symfony2plugin.templating;
 
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementResolveResult;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiPolyVariantReferenceBase;
 import com.intellij.psi.ResolveResult;
+import com.intellij.util.IncorrectOperationException;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import fr.adrienbrault.idea.symfony2plugin.templating.util.TemplateMoveRenameUtil;
 import fr.adrienbrault.idea.symfony2plugin.templating.util.TwigUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,5 +36,31 @@ public class TemplateReference extends PsiPolyVariantReferenceBase<PsiElement> {
     public ResolveResult[] multiResolve(boolean incompleteCode) {
         return PsiElementResolveResult
             .createResults(TwigUtil.getTemplatePsiElements(getElement().getProject(), templateName));
+    }
+
+    /**
+     * Called by IntelliJ's move-file refactoring to update the template path when the referenced
+     * Twig file is moved to a new location.
+     */
+    @Override
+    public PsiElement bindToElement(@NotNull PsiElement element) throws IncorrectOperationException {
+        if (!(element instanceof PsiFile newFile)) {
+            return getElement();
+        }
+
+        VirtualFile newVirtualFile = newFile.getVirtualFile();
+        if (newVirtualFile == null) {
+            return getElement();
+        }
+
+        String newTemplateName = TemplateMoveRenameUtil.resolveNewTemplateName(
+            getElement().getProject(), newVirtualFile, templateName
+        );
+        if (newTemplateName == null) {
+            return getElement();
+        }
+
+        PsiElement result = TemplateMoveRenameUtil.applyToStringLiteralElement(getElement(), newTemplateName);
+        return result != null ? result : getElement();
     }
 }
