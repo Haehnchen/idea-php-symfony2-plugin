2ba7a4bf6294f55e1c3ef66de78482ca89502064
Daniel Espendiller <daniel@espendiller.net>
2026-03-22 15:05:31 +0100

    Skip PHP 8 attributes in `TemplateAnnotationReferences` to avoid duplicate handling with Symfony named-arg provider

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/twig/annotation/TemplateAnnotationReferences.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/twig/annotation/TemplateAnnotationReferences.java
@@ -1,6 +1,8 @@
 package fr.adrienbrault.idea.symfony2plugin.twig.annotation;
 
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.php.lang.psi.elements.PhpAttribute;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import de.espend.idea.php.annotation.extension.PhpAnnotationReferenceProvider;
 import de.espend.idea.php.annotation.extension.parameter.AnnotationPropertyParameter;
@@ -27,7 +29,10 @@ public class TemplateAnnotationReferences implements PhpAnnotationReferenceProvi
             // @Foo("template.html.twig")
             return new PsiReference[]{ new TemplateReference((StringLiteralExpression) annotationPropertyParameter.getElement()) };
         } else if(annotationPropertyParameter.getType() == AnnotationPropertyParameter.Type.PROPERTY_VALUE || "template".equals(annotationPropertyParameter.getPropertyName())) {
-            // @Foo(template="template.html.twig")
+            // @Foo(template="template.html.twig") — skip PHP 8 attributes; handled by SymfonyPhpReferenceContributor generic named-arg provider
+            if (PsiTreeUtil.getParentOfType(annotationPropertyParameter.getElement(), PhpAttribute.class) != null) {
+                return new PsiReference[0];
+            }
             return new PsiReference[]{ new TemplateReference((StringLiteralExpression) annotationPropertyParameter.getElement()) };
         }
 
