9d548b15c216ed5fa2e51b57e407768561e31702
Daniel Espendiller <daniel@espendiller.net>
2026-03-30 10:07:08 +0200

    Remove `ServiceContainerLoader` extension point and inline container file loading logic

--- src/main/java/fr/adrienbrault/idea/symfony2plugin/extension/ServiceContainerLoader.java
+++ src/main/java/fr/adrienbrault/idea/symfony2plugin/extension/ServiceContainerLoader.java
@@ -2,7 +2,9 @@ package fr.adrienbrault.idea.symfony2plugin.extension;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
+ * @deprecated Extension point removed
  */
+@Deprecated
 public interface ServiceContainerLoader {
     void attachContainerFile(ServiceContainerLoaderParameter parameter);
 }
