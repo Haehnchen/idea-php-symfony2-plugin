4f1e9fd33e304003162e4c2b710b3ef666f3c79a
Daniel Espendiller <daniel@espendiller.net>
2026-04-07 19:58:08 +0200

    Migrate ConfigUtilTempTest to light fixture test case

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/config/utils/ConfigUtilTempTest.java
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/config/utils/ConfigUtilTempTest.java
@@ -1,20 +1,20 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.config.utils;
 
 import fr.adrienbrault.idea.symfony2plugin.config.utils.ConfigUtil;
-import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyTempCodeInsightFixtureTestCase;
+import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
  * @see fr.adrienbrault.idea.symfony2plugin.config.utils.ConfigUtil
  */
-public class ConfigUtilTempTest extends SymfonyTempCodeInsightFixtureTestCase {
+public class ConfigUtilTempTest extends SymfonyLightCodeInsightFixtureTestCase {
     /**
      * @see ConfigUtil#getConfigurations
      */
     public void testGetConfigurations() {
-        createFile("config/packages/twig.yml");
-        createFile("config/packages/twig/config.yaml");
-        createFile("app/config/config_dev.yml");
+        createFileInProjectRoot("config/packages/twig.yml", "");
+        createFileInProjectRoot("config/packages/twig/config.yaml", "");
+        createFileInProjectRoot("app/config/config_dev.yml", "");
 
         assertEquals(3, ConfigUtil.getConfigurations(getProject(), "twig").size());
     }
