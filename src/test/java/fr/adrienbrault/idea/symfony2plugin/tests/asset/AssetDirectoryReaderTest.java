f6be1b36fdcf031335589e2d2c6ec90e23149c79
Daniel Espendiller <daniel@espendiller.net>
2026-04-07 18:07:54 +0200

    Refactor AssetDirectoryReaderTest to use light fixture

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/asset/AssetDirectoryReaderTest.java
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/asset/AssetDirectoryReaderTest.java
@@ -1,42 +1,46 @@
 package fr.adrienbrault.idea.symfony2plugin.tests.asset;
 
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.VfsTestUtil;
 import fr.adrienbrault.idea.symfony2plugin.asset.AssetDirectoryReader;
-import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyTempCodeInsightFixtureTestCase;
+import fr.adrienbrault.idea.symfony2plugin.tests.SymfonyLightCodeInsightFixtureTestCase;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
-public class AssetDirectoryReaderTest extends SymfonyTempCodeInsightFixtureTestCase {
+public class AssetDirectoryReaderTest extends SymfonyLightCodeInsightFixtureTestCase {
     public void testSimpleFileResolving() {
-        createFile("web/test.js");
-        createFile("web/foobar/foo/foobar.js");
+        VirtualFile webDirectory = VfsTestUtil.createDir(getProject().getBaseDir(), "web");
+        VfsTestUtil.createFile(webDirectory, "test.js");
+        VirtualFile fooDirectory = VfsTestUtil.createDir(webDirectory, "foobar/foo");
+        VfsTestUtil.createFile(fooDirectory, "foobar.js");
 
         assertNotNull(new AssetDirectoryReader()
             .resolveAssetFile(getProject(), "test.js").stream()
             .filter(virtualFile -> "test.js".equals(virtualFile.getName()))
             .findFirst()
-            .orElseGet(null)
+            .orElse(null)
         );
 
         assertNotNull(new AssetDirectoryReader()
             .resolveAssetFile(getProject(), "foobar///foo\\/foobar.js").stream()
             .filter(virtualFile -> "foobar.js".equals(virtualFile.getName()))
             .findFirst()
-            .orElseGet(null)
+            .orElse(null)
         );
 
         assertNotNull(new AssetDirectoryReader()
             .resolveAssetFile(getProject(), "foobar///foo\\/*").stream()
             .filter(virtualFile -> virtualFile.isDirectory() && "foo".equals(virtualFile.getName()))
             .findFirst()
-            .orElseGet(null)
+            .orElse(null)
         );
 
         assertNotNull(new AssetDirectoryReader()
             .resolveAssetFile(getProject(), "foobar///foo\\/*.js").stream()
             .filter(virtualFile -> virtualFile.isDirectory() && "foo".equals(virtualFile.getName()))
             .findFirst()
-            .orElseGet(null)
+            .orElse(null)
         );
     }
 }
