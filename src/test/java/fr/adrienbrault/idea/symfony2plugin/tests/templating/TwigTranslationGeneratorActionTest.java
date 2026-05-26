240c2bc3b2abaded3a88f863a91780b2c800d46f
Daniel Espendiller <daniel@espendiller.net>
2026-05-26 19:58:37 +0200

    Extract `createTranslationSnippet` method and add corresponding unit tests

--- src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/templating/TwigTranslationGeneratorActionTest.java
+++ src/test/java/fr/adrienbrault/idea/symfony2plugin/tests/templating/TwigTranslationGeneratorActionTest.java
@@ -13,4 +13,18 @@ public class TwigTranslationGeneratorActionTest extends SymfonyLightCodeInsightF
 
         assertTrue(myFixture.testAction(new TwigTranslationGeneratorAction()).isEnabledAndVisible());
     }
+
+    public void testCreateTranslationSnippetForDefaultDomain() {
+        assertEquals(
+            "{{ 'symfony_message'|trans }}",
+            TwigTranslationGeneratorAction.createTranslationSnippet("symfony_message", "messages", "messages")
+        );
+    }
+
+    public void testCreateTranslationSnippetForExplicitDomain() {
+        assertEquals(
+            "{{ 'symfony.great'|trans({}, 'symfony') }}",
+            TwigTranslationGeneratorAction.createTranslationSnippet("symfony.great", "messages", "symfony")
+        );
+    }
 }
