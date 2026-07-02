4f0cffbe661baf034d06ecec01790a5350deef84
Daniel Espendiller <daniel@espendiller.net>
2026-07-02 18:08:55 +0200

    Update Kotlin and IntelliJ Platform plugin versions to 2.4.0 and 2.17.0 respectively

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.3.21"
-    id("org.jetbrains.intellij.platform") version "2.16.0"
+    id("org.jetbrains.kotlin.jvm") version "2.4.0"
+    id("org.jetbrains.intellij.platform") version "2.17.0"
     id("org.jetbrains.changelog") version "2.5.0"
 }
 
